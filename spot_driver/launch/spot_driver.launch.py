efc7b86725ffbcd05f7e7c5343a238cb0635c052
Maximilllian Kirsch <m.kirsch@fh-aachen.de>
2022-06-10 14:35:43 +0200

    added robot_state_publisher from urdf to visualize model in rviz2

--- spot_driver/launch/spot_driver.launch.py
+++ spot_driver/launch/spot_driver.launch.py
@@ -3,8 +3,15 @@ from launch.substitutions import Command, LaunchConfiguration
 from ament_index_python.packages import get_package_share_directory
 import launch_ros
 import os
+from launch_ros.substitutions import FindPackageShare
+import xacro
 
 def generate_launch_description():
+    pkg_share = FindPackageShare('spot_description').find('spot_description')
+    urdf_dir = os.path.join(pkg_share, 'urdf')
+    xacro_file = os.path.join(urdf_dir, 'spot.urdf.xacro')
+    doc = xacro.process_file(xacro_file)
+    robot_desc = doc.toprettyxml(indent='  ')
 
     config_login = os.path.join(
         get_package_share_directory('spot_driver'),
@@ -24,6 +31,14 @@ def generate_launch_description():
         parameters=[config_login,config_ros],
     )
 
+    params = {'robot_description': robot_desc}
+    robot_state_publisher = launch_ros.actions.Node(
+                                package='robot_state_publisher',
+                                executable='robot_state_publisher',
+                                output='screen',
+                                parameters=[params])
+
     return launch.LaunchDescription([
-        spot_driver_node
+        spot_driver_node,
+        robot_state_publisher
     ])
