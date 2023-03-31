70124b76eb1bd959fc9d37264d5d92fbc8e80e5a
Jenny Barry <jbarry@theaiinstitute.com>
2023-03-31 16:46:32 -0400

    An arm example that is a direct conversion of the BD arm_simple example

---
+++ examples/simple_arm_motion/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'simple_arm_motion'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='BD AI Institute',
+    maintainer_email='engineering@theaiinstitute.com',
+    description='Direct conversion of BD simple arm command example',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'arm_simple = simple_arm_motion.arm_simple:main',
+        ],
+    },
+)
