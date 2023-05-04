27a174ab0450f06fbf0fc149860a2408b4093b22
Kaiyu Zheng <kzheng@theaiinstitute.com>
2023-05-04 19:12:30 -0400

    remove publish_x_args to enforce config of those through config_file

--- spot_driver/launch/spot_driver_with_namespace.launch.py
+++ spot_driver/launch/spot_driver_with_namespace.launch.py
@@ -20,27 +20,6 @@ def generate_launch_description():
     doc = xacro.process_file(xacro_file)
     robot_desc = doc.toprettyxml(indent='  ')
 
-    publish_rgb = LaunchConfiguration("publish_rgb", default="true")
-    publish_rgb_arg = DeclareLaunchArgument(
-        "publish_rgb",
-        description="Start publishing all RGB channels on Spot cameras",
-        default_value="true",
-    )
-
-    publish_depth = LaunchConfiguration("publish_depth", default="true")
-    publish_depth_arg = DeclareLaunchArgument(
-        "publish_depth",
-        description="Start publishing all depth channels on Spot cameras",
-        default_value="true",
-    )
-
-    publish_depth_registered = LaunchConfiguration("publish_depth_registered", default="false")
-    publish_depth_registered_arg = DeclareLaunchArgument(
-        "publish_depth_registered",
-        description="Start publishing all depth_registered channels on Spot cameras",
-        default_value="false",
-    )
-
     spot_driver_node = launch_ros.actions.Node(
         package='spot_driver',
         executable='spot_ros2',
@@ -59,9 +38,6 @@ def generate_launch_description():
         parameters=[params])
 
     return launch.LaunchDescription([
-        publish_rgb_arg,
-        publish_depth_arg,
-        publish_depth_registered_arg,
         spot_name_arg,
         config_file_arg,
         spot_driver_node,
