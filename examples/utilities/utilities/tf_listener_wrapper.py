fb804813f5044882a8318ca0e74e4d768b48cfd3
Jenny Barry <jbarry@theaiinstitute.com>
2023-05-26 14:04:24 -0400

    Add another missing import

--- examples/utilities/utilities/tf_listener_wrapper.py
+++ examples/utilities/utilities/tf_listener_wrapper.py
@@ -2,7 +2,7 @@
 
 import bdai_ros2_wrappers.tf_listener_wrapper as tfl
 from geometry_msgs.msg import Transform
-from bosdyn.client.math_helpers import SE3Pose
+from bosdyn.client.math_helpers import SE3Pose, Quat
 
 def ros_transform_to_se3_pose(transform: Transform) -> SE3Pose:
     return SE3Pose(transform.translation.x, transform.translation.y, transform.translation.z,
