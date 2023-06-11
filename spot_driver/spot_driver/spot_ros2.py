231fdf8227900dd341ab6a0b8fc46dc080dfd7a6
Kaiyu Zheng <kzheng@theaiinstitute.com>
2023-06-11 16:33:00 -0400

    be specific about the added case

--- spot_driver/spot_driver/spot_ros2.py
+++ spot_driver/spot_driver/spot_ros2.py
@@ -1260,11 +1260,14 @@ class SpotROS(Node):
                 elif mob_feedback.feedback.feedback_choice == mob_feedback.feedback.FEEDBACK_FOLLOW_ARM_FEEDBACK_SET:
                     self.get_logger().warn("WARNING: FollowArmCommand provides no feedback")
                     pass  # May return SUCCESS below
-                else:
+                elif mob_feedback.feedback.feedback_choice == mob_feedback.feedback.FEEDBACK_NOT_SET:
                     # sync_feedback.mobility_command_feedback_is_set, feedback_choice is actually not set.
                     # This may happen when a command finishes, which means we may return SUCCESS below.
                     self.get_logger().info("mobility command feedback indicates goal has reached")
                     pass
+                else:
+                    self.get_logger().error("ERROR: unknown mobility command type")
+                    return GoalResponse.IN_PROGRESS
 
             if sync_feedback.gripper_command_feedback_is_set is True:
                 grip_feedback = sync_feedback.gripper_command_feedback
