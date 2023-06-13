a78e052a5ff4231bfd35c7ce57907983b5db64bc
Kaiyu Zheng <kzheng@theaiinstitute.com>
2023-06-13 23:12:39 +0000

    black

--- spot_driver/launch/spot_driver.launch.py
+++ spot_driver/launch/spot_driver.launch.py
@@ -9,9 +9,7 @@ from launch.substitutions import LaunchConfiguration
 from launch_ros.substitutions import FindPackageShare
 
 
-def launch_robot_state_publisher(
-    context: LaunchContext, has_arm: LaunchConfiguration, ld: LaunchDescription
-) -> None:
+def launch_robot_state_publisher(context: LaunchContext, has_arm: LaunchConfiguration, ld: LaunchDescription) -> None:
     pkg_share = FindPackageShare("spot_description").find("spot_description")
     urdf_dir = os.path.join(pkg_share, "urdf")
 
