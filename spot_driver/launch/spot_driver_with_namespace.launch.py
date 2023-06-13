0ad421730c1f115a4126864d3474c6f16b7490d3
Kaiyu Zheng <kzheng@theaiinstitute.com>
2023-06-13 16:31:56 -0400

    adding 'has_arm' option to spot driver with namespace launch file

--- spot_driver/launch/spot_driver_with_namespace.launch.py
+++ spot_driver/launch/spot_driver_with_namespace.launch.py
@@ -3,23 +3,57 @@ import os
 import launch
 import launch_ros
 import xacro
-from launch.actions import DeclareLaunchArgument
+from launch import LaunchContext, LaunchDescription
+from launch.actions import DeclareLaunchArgument, GroupAction, OpaqueFunction
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
+
+def launch_robot_state_publisher(
+        context: LaunchContext,
+        spot_name: LaunchConfiguration,
+        has_arm: LaunchConfiguration,
+        ld: LaunchDescription):
+    pkg_share = FindPackageShare("spot_description").find("spot_description")
+    urdf_dir = os.path.join(pkg_share, "urdf")
+
+    has_arm = has_arm.perform(context)
+    if has_arm:
+        xacro_file = os.path.join(urdf_dir, "spot_with_arm.urdf.xacro")
+    else:
+        xacro_file = os.path.join(urdf_dir, "spot.urdf.xacro")
+    doc = xacro.process_file(xacro_file)
+    robot_desc = doc.toprettyxml(indent="  ")
+
+    params = {"robot_description": robot_desc, "frame_prefix": PathJoinSubstitution([spot_name, ""])}
+    robot_state_publisher = launch_ros.actions.Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="screen",
+        namespace=spot_name,
+        parameters=[params],
+    )
+    ld.add_action(robot_state_publisher)
 
 
 def generate_launch_description() -> launch.LaunchDescription:
     spot_name = LaunchConfiguration("spot_name")
     spot_name_arg = DeclareLaunchArgument("spot_name", description="Name of spot")
 
+    has_arm = LaunchConfiguration("has_arm")
+    has_arm_arg = DeclareLaunchArgument("has_arm",
+                                        description="Name of spot",
+                                        default_value="False")
+
     config_file = LaunchConfiguration("config_file")
     config_file_arg = DeclareLaunchArgument("config_file", description="Path to configuration file for the driver.")
 
-    pkg_share = FindPackageShare("spot_description").find("spot_description")
-    urdf_dir = os.path.join(pkg_share, "urdf")
-    xacro_file = os.path.join(urdf_dir, "spot.urdf.xacro")
-    doc = xacro.process_file(xacro_file)
-    robot_desc = doc.toprettyxml(indent="  ")
+    ld = launch.LaunchDescription(
+        [
+            spot_name_arg,
+            config_file_arg,
+            has_arm_arg
+        ]
+    )
 
     spot_driver_node = launch_ros.actions.Node(
         package="spot_driver",
@@ -30,20 +64,6 @@ def generate_launch_description() -> launch.LaunchDescription:
         parameters=[config_file, {"spot_name": spot_name}],
     )
 
-    params = {"robot_description": robot_desc, "frame_prefix": PathJoinSubstitution([spot_name, ""])}
-    robot_state_publisher = launch_ros.actions.Node(
-        package="robot_state_publisher",
-        executable="robot_state_publisher",
-        output="screen",
-        namespace=spot_name,
-        parameters=[params],
-    )
-
-    return launch.LaunchDescription(
-        [
-            spot_name_arg,
-            config_file_arg,
-            spot_driver_node,
-            robot_state_publisher,
-        ]
-    )
+    ld.add_action(OpaqueFunction(
+        function=launch_robot_state_publisher, args=[spot_name, has_arm, ld]))
+    return ld
