0f024a39391c721fd976a13374044b433f97e1db
Kaiyu Zheng <kzheng@theaiinstitute.com>
2023-06-13 18:16:19 -0400

    correction; correctly convert string to bool

--- spot_driver/launch/spot_driver_with_namespace.launch.py
+++ spot_driver/launch/spot_driver_with_namespace.launch.py
@@ -16,7 +16,7 @@ def launch_robot_state_publisher(
     pkg_share = FindPackageShare("spot_description").find("spot_description")
     urdf_dir = os.path.join(pkg_share, "urdf")
 
-    has_arm = bool(has_arm.perform(context))
+    has_arm = has_arm.perform(context) == "True"
     if has_arm:
         xacro_file = os.path.join(urdf_dir, "spot_with_arm.urdf.xacro")
     else:
