1b0285b2a022510dfe4e06f70d5f0e055b8bd032
Giovanni Remigi <gremigi@theaiinstitute.com>
2023-11-28 18:13:16 +0100

    Unit tests.

--- spot_driver_cpp/test/src/conversions/test_kinematic_conversions.cpp
+++ spot_driver_cpp/test/src/conversions/test_kinematic_conversions.cpp
@@ -7,11 +7,22 @@
 
 namespace spot_ros2 {
 
-TEST(TestKinematicConversions, my_test) {
-  bosdyn_msgs::msg::InverseKinematicsRequest ros_msg;
+TEST(TestKinematicConversions, convert_bosdyn_msgs_inverse_kinematics_request_to_proto) {
+  bosdyn_msgs::msg::InverseKinematicsRequest ros;
+  ros.header.client_name = "test_client";
+  ros.header_is_set = true;
+  ros.header.request_timestamp.sec = 2;
+  ros.header.request_timestamp.nanosec = 200;
+  ros.header.request_timestamp_is_set = true;
   bosdyn::api::spot::InverseKinematicsRequest proto;
 
-  kinematic_conversions::convert_bosdyn_msgs_inverse_kinematics_request_to_proto(ros_msg, proto);
+  kinematic_conversions::convert_bosdyn_msgs_inverse_kinematics_request_to_proto(ros, proto);
+
+  ASSERT_EQ(ros.header_is_set, proto.has_header());
+  ASSERT_EQ(ros.header.client_name, proto.header().client_name());
+  ASSERT_EQ(ros.header.request_timestamp_is_set, proto.header().has_request_timestamp());
+  ASSERT_EQ(ros.header.request_timestamp.sec, proto.header().request_timestamp().seconds());
+  ASSERT_EQ(ros.header.request_timestamp.nanosec, proto.header().request_timestamp().nanos());
 }
 
 }  // namespace spot_ros2
