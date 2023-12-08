21aa6e2b2f4eb95069d6ce6a65e3f2b89c949539
Giovanni Remigi <gremigi@theaiinstitute.com>
2023-12-07 17:16:36 -0700

    send_inverse_kinematics_requests.py updated.

--- examples/inverse_kinematics/inverse_kinematics/send_inverse_kinematics_requests.py
+++ examples/inverse_kinematics/inverse_kinematics/send_inverse_kinematics_requests.py
@@ -4,19 +4,25 @@
 
 import argparse
 
+import numpy as np
+
 import bdai_ros2_wrappers.process as ros_process
 import bdai_ros2_wrappers.scope as ros_scope
 import bosdyn_msgs.msg
 import geometry_msgs.msg
 from bdai_ros2_wrappers.utilities import namespace_with
-from bosdyn.client.frame_helpers import GRAV_ALIGNED_BODY_FRAME_NAME, ODOM_FRAME_NAME
+from bosdyn.client.frame_helpers import GRAV_ALIGNED_BODY_FRAME_NAME, GROUND_PLANE_FRAME_NAME, ODOM_FRAME_NAME
 from spot_utilities.spot_basic import SpotBasic
 from utilities.tf_listener_wrapper import TFListenerWrapper
+
+from bosdyn.client.math_helpers import Quat, SE3Pose
 
 import spot_msgs.srv
 
 
-def create_kinematic_request() -> spot_msgs.srv.GetInverseKinematicSolutions.Request:
+def create_kinematic_request(
+    odom_T_task: SE3Pose, wr1_T_tool: SE3Pose, task_T_desired_tool: SE3Pose
+) -> spot_msgs.srv.GetInverseKinematicSolutions.Request:
 
     # Task frame.
     task_frame = geometry_msgs.msg.Pose()
@@ -78,11 +84,16 @@ def send_requests(robot_name: str, poses: int) -> bool:
         raise ValueError("no ROS 2 node available (did you use bdai_ros2_wrapper.process.main?)")
     logger = node.get_logger()
 
+    # Frame names.
     odom_frame_name = namespace_with(robot_name, ODOM_FRAME_NAME)
-    grav_aligned_body_frame_name = namespace_with(robot_name, GRAV_ALIGNED_BODY_FRAME_NAME)
-    tf_listener = TFListenerWrapper(node)
-    tf_listener.wait_for_a_tform_b(odom_frame_name, grav_aligned_body_frame_name)
+    flat_body_frame_name = namespace_with(robot_name, GRAV_ALIGNED_BODY_FRAME_NAME)
+    ground_plane_frame_name = namespace_with(robot_name, GROUND_PLANE_FRAME_NAME)
 
+    # Wait for the robot to publish the TF state.
+    tf_listener = TFListenerWrapper(node)
+    tf_listener.wait_for_a_tform_b(odom_frame_name, flat_body_frame_name)
+
+    # Instantiate a facade to send commands to the robot.
     robot = SpotBasic(robot_name)
 
     # Claim robot.
@@ -108,7 +119,40 @@ def send_requests(robot_name: str, poses: int) -> bool:
         return False
     logger.info("Successfully stood up.")
 
-    request = create_kinematic_request()
+    ############################################
+
+    odom_T_flat_body: SE3Pose = tf_listener.lookup_a_tform_b(odom_frame_name, flat_body_frame_name)
+    odom_T_gpe: SE3Pose = tf_listener.lookup_a_tform_b(odom_frame_name, ground_plane_frame_name)
+
+    # Construct the frame on the ground right underneath the center of the body.
+    odom_T_ground_body: SE3Pose = odom_T_flat_body
+    odom_T_ground_body.z = odom_T_gpe.z
+
+    # Now, construct a task frame slightly above the ground, in front of the robot.
+    odom_T_task: SE3Pose = odom_T_ground_body * SE3Pose(x=0.4, y=0, z=0.05, rot=Quat(w=1, x=0, y=0, z=0))
+
+    # Now, let's set our tool frame to be the tip of the robot's bottom jaw. Flip the
+    # orientation so that when the hand is pointed downwards, the tool's z-axis is
+    # pointed upward.
+    wr1_T_tool: SE3Pose = SE3Pose(0.23589, 0, -0.03943, Quat.from_pitch(-np.pi / 2))
+
+    # Generate several random poses relative to the task frame.
+    rng = np.random.seed(0)
+    x_size = 0.7  # m
+    y_size = 0.8  # m
+    x_rt_task = x_size * rng.random(poses)
+    y_rt_task = -y_size / 2 + y_size * rng.random(poses)
+    task_T_desired_tools = [
+        SE3Pose(xi_rt_task, yi_rt_task, 0.0, Quat())
+        for (xi_rt_task, yi_rt_task) in zip(x_rt_task.flatten(), y_rt_task.flatten())
+    ]
+    task_T_desired_tools = task_T_desired_tools[0]
+
+    # Create a target pose.
+    task_T_desired_tool = SE3Pose(0, 0, 0, Quat.from_pitch(-np.pi / 2))
+    request = create_kinematic_request(
+        odom_T_task=odom_T_task, wr1_T_tool=wr1_T_tool, task_T_desired_tool=task_T_desired_tool
+    )
 
     # Send inverse kinematic request.
     kinematics_client = node.create_client(
