c5e760d75c38efb5ab51de76ef18d2a37e0f9175
Giovanni Remigi <gremigi@theaiinstitute.com>
2023-12-22 22:06:45 +0100

    Added test_kinematic_service.

---
+++ spot_driver_cpp/test/include/spot_driver_cpp/mock/mock_kinematic_api.hpp
@@ -0,0 +1,15 @@
+// Copyright (c) 2023 Boston Dynamics AI Institute LLC. All rights reserved.
+
+#include <gmock/gmock.h>
+
+#include <spot_driver_cpp/api/kinematic_api.hpp>
+#include <spot_driver_cpp/kinematic/kinematic_service.hpp>
+
+namespace spot_ros2::test {
+
+class MockKinematicApi : public KinematicApi {
+  MOCK_METHOD((tl::expected<Result<InverseKinematicsResponse>, std::string>), get_solutions,
+              (InverseKinematicsRequest & request), (override));
+};
+
+}  // namespace spot_ros2::test
--- spot_driver_cpp/test/src/kinematic/test_kinematic_service.cpp
+++ spot_driver_cpp/test/src/kinematic/test_kinematic_service.cpp
@@ -2,6 +2,35 @@
 
 #include <gmock/gmock.h>
 
-#include <spot_driver_cpp/api/kinematic_api.hpp>
+#include <memory>
 
-namespace spot_ros2::kinematic::test {}  // namespace spot_ros2::kinematic::test
+#include <spot_driver_cpp/api/kinematic_api.hpp>
+#include <spot_driver_cpp/kinematic/kinematic_service.hpp>
+#include <spot_driver_cpp/mock/mock_kinematic_api.hpp>
+#include <spot_driver_cpp/mock/mock_logger_interface.hpp>
+
+namespace spot_ros2::kinematic::test {
+
+using ::testing::_;
+
+class MockMiddlewareHandle : public KinematicService::MiddlewareHandle {
+ public:
+  MOCK_METHOD((std::shared_ptr<rclcpp::Service<GetInverseKinematicSolutions>>), create_service,
+              (std::string service_name,
+               std::function<void(const std::shared_ptr<GetInverseKinematicSolutions::Request>,
+                                  std::shared_ptr<GetInverseKinematicSolutions::Response>)>
+                   callback),
+              (override));
+};
+
+TEST(TestKinematicService, initialize) {
+  auto ik_api = std::make_unique<spot_ros2::test::MockKinematicApi>();
+  auto logger = std::make_shared<spot_ros2::test::MockLoggerInterface>();
+  auto middleware = std::make_unique<MockMiddlewareHandle>();
+  EXPECT_CALL(*middleware, create_service(_, _)).Times(1);
+
+  auto ik_service = std::make_unique<KinematicService>(std::move(ik_api), logger, std::move(middleware));
+  ik_service->initialize();
+}
+
+}  // namespace spot_ros2::kinematic::test
