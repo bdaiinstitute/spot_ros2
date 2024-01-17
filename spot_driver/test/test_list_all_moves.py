c9300c8a365c5ce0693100780f1e37e1b1954306
Giovanni Remigi <gremigi@theaiinstitute.com>
2024-01-17 17:13:17 +0100

    Added "test_list_all_moves.py".

--- spot_driver/test/test_list_all_moves.py
+++ spot_driver/test/test_list_all_moves.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2023 Boston Dynamics AI Institute LLC. See LICENSE file for more info.
 
 """
-Test for the List All Dances command.
+Test for the List All Moves command.
 """
 
 # We disable Pylint warnings for all Protobuf files which contain objects with
@@ -11,16 +11,16 @@ Test for the List All Dances command.
 import pytest
 from bdai_ros2_wrappers.futures import wait_for_future
 from bdai_ros2_wrappers.scope import ROSAwareScope
-from bosdyn.api.spot.choreography_sequence_pb2 import ListAllSequencesResponse
+from bosdyn.api.spot.choreography_sequence_pb2 import ListAllMovesResponse
 
-from spot_msgs.srv import ListAllDances  # type: ignore
+from spot_msgs.srv import ListAllMoves  # type: ignore
 from spot_wrapper.testing.fixtures import SpotFixture
 
 
 @pytest.mark.usefixtures("spot_node")
-def test_list_all_dances(ros: ROSAwareScope, simple_spot: SpotFixture) -> None:
+def test_list_all_moves(ros: ROSAwareScope, simple_spot: SpotFixture) -> None:
     """
-    This integration test checks if the "list_all_dances" service infrastructure is
+    This integration test checks if the "list_all_moves" service infrastructure is
     setup correctly.
 
     Args:
@@ -30,17 +30,17 @@ def test_list_all_dances(ros: ROSAwareScope, simple_spot: SpotFixture) -> None:
     """
 
     # Send ROS request.
-    client = ros.node.create_client(ListAllDances, "list_all_dances")
-    request = ListAllDances.Request()
+    client = ros.node.create_client(ListAllMoves, "list_all_moves")
+    request = ListAllMoves.Request()
     future = client.call_async(request)
 
     # Mock GRPC sever.
 
-    # Serve "list_all_sequences" command.
-    list_sequences_call = simple_spot.api.ListAllSequences.serve(timeout=2.0)
-    assert list_sequences_call is not None
-    list_sequences_response = ListAllSequencesResponse()
-    list_sequences_call.returns(list_sequences_response)
+    # Serve "list_all_moves" command.
+    list_moves_call = simple_spot.api.ListAllMoves.serve(timeout=2.0)
+    assert list_moves_call is not None
+    list_moves_response = ListAllMovesResponse()
+    list_moves_call.returns(list_moves_response)
 
     # Wait for ROS response.
     assert wait_for_future(future, timeout_sec=2.0)
