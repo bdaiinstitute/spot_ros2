3ad7ab259d77617f4eb65007f347edb37f8b33d1
Joe Schornak <jschornak@theaiinstitute.com>
2024-01-17 16:52:04 -0500

    add include for rclcpp/node_options.hpp as workaround for upstream bug

--- spot_driver_cpp/src/robot_state/spot_robot_state_publisher_component.cpp
+++ spot_driver_cpp/src/robot_state/spot_robot_state_publisher_component.cpp
@@ -1,5 +1,6 @@
 // Copyright (c) 2023 Boston Dynamics AI Institute LLC. All rights reserved.
 
+#include <rclcpp/node_options.hpp>
 #include <rclcpp_components/register_node_macro.hpp>
 #include <spot_driver_cpp/robot_state/spot_robot_state_publisher_node.hpp>
 
