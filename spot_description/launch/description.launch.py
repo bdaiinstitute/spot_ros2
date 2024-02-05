41971e3d3e13121f90c02d9dddd4d36fd05d8eb2
daviddorf2023 <daviddorf2023@u.northwestern.edu>
2024-02-05 18:07:10 -0500

    Style fixes

--- spot_description/launch/description.launch.py
+++ spot_description/launch/description.launch.py
@@ -1,4 +1,5 @@
 import os
+
 import launch
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import Command, LaunchConfiguration
@@ -7,8 +8,7 @@ from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description() -> launch.LaunchDescription:
-    pkg_share = FindPackageShare(
-        package="spot_description").find("spot_description")
+    pkg_share = FindPackageShare(package="spot_description").find("spot_description")
     default_model_path = os.path.join(pkg_share, "urdf/spot.urdf.xacro")
     default_rviz2_path = os.path.join(pkg_share, "rviz/viz_spot.rviz")
     return launch.LaunchDescription(
@@ -22,40 +22,43 @@ def generate_launch_description() -> launch.LaunchDescription:
             DeclareLaunchArgument(
                 name="rvizconfig", default_value=default_rviz2_path, description="Absolute path to rviz config file"
             ),
-            DeclareLaunchArgument(
-                name="arm", default_value="False", description="Flag to enable arm"
-            ),
+            DeclareLaunchArgument(name="arm", default_value="False", description="Flag to enable arm"),
             DeclareLaunchArgument(
                 "tf_prefix", default_value='""', description="Apply namespace prefix to robot links and joints"
             ),
-            DeclareLaunchArgument(
-                "namespace", default_value="", description="Namespace for robot tf topic"),
+            DeclareLaunchArgument("namespace", default_value="", description="Namespace for robot tf topic"),
             Node(
                 package="robot_state_publisher",
                 executable="robot_state_publisher",
-                parameters=[{"robot_description": Command(
-                    ["xacro ", LaunchConfiguration("model"), " arm:=", LaunchConfiguration('arm'),
-                     " tf_prefix:=", LaunchConfiguration('tf_prefix')])}],
-                namespace=LaunchConfiguration(
-                    "namespace"),
+                parameters=[
+                    {
+                        "robot_description": Command(
+                            [
+                                "xacro ",
+                                LaunchConfiguration("model"),
+                                " arm:=",
+                                LaunchConfiguration("arm"),
+                                " tf_prefix:=",
+                                LaunchConfiguration("tf_prefix"),
+                            ]
+                        )
+                    }
+                ],
+                namespace=LaunchConfiguration("namespace"),
             ),
             Node(
                 package="joint_state_publisher",
                 executable="joint_state_publisher",
                 name="joint_state_publisher",
-                condition=launch.conditions.UnlessCondition(
-                    LaunchConfiguration("gui")),
-                namespace=LaunchConfiguration(
-                    "namespace"),
+                condition=launch.conditions.UnlessCondition(LaunchConfiguration("gui")),
+                namespace=LaunchConfiguration("namespace"),
             ),
             Node(
                 package="joint_state_publisher_gui",
                 executable="joint_state_publisher_gui",
                 name="joint_state_publisher_gui",
-                condition=launch.conditions.IfCondition(
-                    LaunchConfiguration("gui")),
-                namespace=LaunchConfiguration(
-                    "namespace"),
+                condition=launch.conditions.IfCondition(LaunchConfiguration("gui")),
+                namespace=LaunchConfiguration("namespace"),
             ),
             Node(
                 package="rviz2",
