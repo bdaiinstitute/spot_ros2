bf5cd409995fce2d1d68a0ffbf7d3bb0e7d7df0b
Katie Hughes <khughes@theaiinstitute.com>
2024-02-16 16:01:57 -0500

    Added a way to list the current world objects

--- examples/set_nogo_region/set_nogo_region/nogo_region.py
+++ examples/set_nogo_region/set_nogo_region/nogo_region.py
@@ -5,16 +5,16 @@ from typing import Optional
 import bdai_ros2_wrappers.process as ros_process
 import bdai_ros2_wrappers.scope as ros_scope
 from bdai_ros2_wrappers.action_client import ActionClientWrapper
+from bdai_ros2_wrappers.futures import wait_for_future
 from bdai_ros2_wrappers.utilities import fqn, namespace_with
 from bosdyn.client.frame_helpers import BODY_FRAME_NAME, VISION_FRAME_NAME
 from bosdyn.client.math_helpers import SE2Pose
-from bosdyn.client.robot_command import RobotCommandBuilder
 from rclpy.node import Node
 from utilities.simple_spot_commander import SimpleSpotCommander
 from utilities.tf_listener_wrapper import TFListenerWrapper
 
-import spot_driver.conversions as conv
 from spot_msgs.action import RobotCommand  # type: ignore
+from spot_msgs.srv import ListWorldObjects  # type: ignore
 
 # Where we want the robot to walk to relative to itself
 ROBOT_T_GOAL = SE2Pose(1.0, 0.0, 0.0)
@@ -26,6 +26,7 @@ class NoGoRegion:
         node = node or ros_scope.node()
         if node is None:
             raise ValueError("no ROS 2 node available (did you use bdai_ros2_wrapper.process.main?)")
+        self.node = node
         self._robot_name = robot_name
 
         self._body_frame_name = namespace_with(self._robot_name, BODY_FRAME_NAME)
@@ -37,6 +38,9 @@ class NoGoRegion:
         self._robot_command_client = ActionClientWrapper(
             RobotCommand, namespace_with(self._robot_name, "robot_command"), node
         )
+        self._list_wo_client = node.create_client(
+            ListWorldObjects, namespace_with(self._robot_name, "list_world_objects")
+        )
 
     def initialize_robot(self) -> bool:
         self._logger.info(f"Robot name: {self._robot_name}")
@@ -46,37 +50,23 @@ class NoGoRegion:
             self._logger.error("Unable to claim robot message was " + result.message)
             return False
         self._logger.info("Claimed robot")
-
-        # Stand the robot up.
         self._logger.info("Powering robot on")
         result = self._robot.command("power_on")
         if not result.success:
             self._logger.error("Unable to power on robot message was " + result.message)
             return False
-        self._logger.info("Standing robot up")
-        result = self._robot.command("stand")
-        if not result.success:
-            self._logger.error("Robot did not stand message was " + result.message)
-            return False
-        self._logger.info("Successfully stood up.")
         return True
 
-    def walk_forward_with_world_frame_goal(self) -> None:
-        self._logger.info("Walking forward")
-        world_t_robot = self._tf_listener.lookup_a_tform_b(
-            self._vision_frame_name, self._body_frame_name
-        ).get_closest_se2_transform()
-        world_t_goal = world_t_robot * ROBOT_T_GOAL
-        proto_goal = RobotCommandBuilder.synchro_se2_trajectory_point_command(
-            goal_x=world_t_goal.x,
-            goal_y=world_t_goal.y,
-            goal_heading=world_t_goal.angle,
-            frame_name=VISION_FRAME_NAME,  # use Boston Dynamics' frame conventions
-        )
-        action_goal = RobotCommand.Goal()
-        conv.convert_proto_to_bosdyn_msgs_robot_command(proto_goal, action_goal.command)
-        self._robot_command_client.send_goal_and_wait("walk_forward", action_goal)
-        self._logger.info("Successfully walked forward")
+    def list_current_world_objects(self) -> None:
+        request = ListWorldObjects.Request()
+        print("Calling list world object")
+        future = self._list_wo_client.call_async(request)
+        if not wait_for_future(future, context=self.node.context):
+            return
+        print("Getting result")
+        response = future.result()
+        for wo in response.response.world_objects:
+            print(f"\n\nID: {wo.id} name: {wo.name}\n{wo}")
 
 
 def cli() -> argparse.ArgumentParser:
@@ -89,7 +79,7 @@ def cli() -> argparse.ArgumentParser:
 def main(args: argparse.Namespace) -> int:
     nogo = NoGoRegion(args.robot, main.node)
     nogo.initialize_robot()
-    nogo.walk_forward_with_world_frame_goal()
+    nogo.list_current_world_objects()
     return 0
 
 
