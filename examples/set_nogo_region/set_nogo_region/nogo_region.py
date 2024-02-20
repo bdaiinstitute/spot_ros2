17f939d8c12b1aa0d977fedad0dc251f473a31c8
Katie Hughes <khughes@theaiinstitute.com>
2024-02-20 10:21:07 -0500

    Sucessfully added and deleted a nogo region on robot

--- examples/set_nogo_region/set_nogo_region/nogo_region.py
+++ examples/set_nogo_region/set_nogo_region/nogo_region.py
@@ -9,7 +9,7 @@ from bdai_ros2_wrappers.futures import wait_for_future
 from bdai_ros2_wrappers.utilities import fqn, namespace_with
 from bosdyn.client.frame_helpers import BODY_FRAME_NAME, VISION_FRAME_NAME
 from bosdyn.client.math_helpers import SE2Pose
-from bosdyn_msgs.msg import MutateWorldObjectRequestAction
+from bosdyn_msgs.msg import MutateWorldObjectRequestAction, MutateWorldObjectResponseStatus
 from rclpy.node import Node
 from utilities.simple_spot_commander import SimpleSpotCommander
 from utilities.tf_listener_wrapper import TFListenerWrapper
@@ -67,7 +67,7 @@ class NoGoRegion:
 
     def list_current_world_objects(self) -> None:
         request = ListWorldObjects.Request()
-        print("Listing world objects:")
+        print("Current world objects:")
         future = self._list_wo_client.call_async(request)
         if not wait_for_future(future, context=self.node.context):
             return
@@ -75,10 +75,10 @@ class NoGoRegion:
         for wo in response.response.world_objects:
             print(f"ID: {wo.id} name: {wo.name}")
             # print out nogo properties
-            if wo.nogo_region_properties_is_set:
-                print(f"\tNogo properties set to: {wo.nogo_region_properties}")
+            # if wo.nogo_region_properties_is_set:
+            #     print(f"\tNogo properties set to: {wo.nogo_region_properties}")
 
-    def add_nogo_region(self, name: str = "nogo_region", lifetime: int = 10) -> bool:
+    def add_nogo_region(self, name: str = "nogo_region", lifetime: int = 10) -> Optional[int]:
         request = MutateWorldObject.Request()
         request.request.mutation.action.value = MutateWorldObjectRequestAction.ACTION_ADD
         request.request.mutation_is_set = True  # TODO If this is false, driver will crash. should handle in driver
@@ -86,24 +86,49 @@ class NoGoRegion:
         # TODO figure out how to make it persist infinitely
         request.request.mutation.object.object_lifetime.sec = lifetime
         request.request.mutation.object.object_lifetime_is_set = True
-        # set NOGO properties. this should set a 1x1 nogo region 2m in front of Spot
+        # set no-go box, here it is a 1x1 region 2m in front of Spot
         request.request.mutation.object.nogo_region_properties.region.box.box.size.x = 1.0
         request.request.mutation.object.nogo_region_properties.region.box.box.size.y = 1.0
         request.request.mutation.object.nogo_region_properties.region.box.box.size_is_set = True
+        # set box's offset from Spot
         request.request.mutation.object.nogo_region_properties.region.box.frame_name = VISION_FRAME_NAME
         request.request.mutation.object.nogo_region_properties.region.box.frame_name_tform_box.position.x = 2.0
         request.request.mutation.object.nogo_region_properties.region.box.frame_name_tform_box_is_set = True
+        request.request.mutation.object.nogo_region_properties.region.box.box_is_set = True
+        # final flags
         request.request.mutation.object.nogo_region_properties.region.region_choice = 1  # TODO 1=set 0=unset
         request.request.mutation.object.nogo_region_properties_is_set = True
         request.request.mutation.object_is_set = True
         request.request.mutation_is_set = True
+        # call request
+        future = self._mutuate_wo_client.call_async(request)
+        if not wait_for_future(future, context=self.node.context):
+            return None
+        response = future.result()
+        print(f"Added a world object with ID {response.response.mutated_object_id}")
+        # TODO check if the world object was added successfully
+        if response.response.status.value == MutateWorldObjectResponseStatus.STATUS_OK:
+            return response.response.mutated_object_id
+        else:
+            return None
+
+    def delete_nogo_region(self, nogo_id: int) -> bool:
+        print(f"Deleting world object with ID {nogo_id}")
+        request = MutateWorldObject.Request()
+        request.request.mutation.action.value = MutateWorldObjectRequestAction.ACTION_DELETE
+        request.request.mutation_is_set = True  # TODO If this is false, driver will crash. should handle in driver
+        # tell it to delete the world object with the given ID
+        request.request.mutation.object.id = nogo_id
+        request.request.mutation.object_is_set = True
+        # call service
         future = self._mutuate_wo_client.call_async(request)
         if not wait_for_future(future, context=self.node.context):
             return False
         response = future.result()
-        print(f"Added a world object with ID {response.response.mutated_object_id}")
-        # TODO check if the world object was added successfully
-        return True
+        if response.response.status.value == MutateWorldObjectResponseStatus.STATUS_OK:
+            return True
+        else:
+            return False
 
 
 def cli() -> argparse.ArgumentParser:
@@ -117,8 +142,11 @@ def main(args: argparse.Namespace) -> int:
     nogo = NoGoRegion(args.robot, main.node)
     nogo.initialize_robot()
     nogo.list_current_world_objects()
-    nogo.add_nogo_region()
+    nogo_id = nogo.add_nogo_region()
     nogo.list_current_world_objects()
+    if nogo_id:
+        nogo.delete_nogo_region(nogo_id)
+        nogo.list_current_world_objects()
     return 0
 
 
