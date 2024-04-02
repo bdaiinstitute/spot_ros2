59c8e3fdd3ae2ca80d15490dbf59ec2411c3859e
Griswald Brooks <gbrooks@theaiinstitute.com>
2024-04-02 19:07:17 -0400

    [PN-60] Recorded some good tuning values

--- spot_stitcher/src/stitch_publisher.cpp
+++ spot_stitcher/src/stitch_publisher.cpp
@@ -2,6 +2,7 @@
 #include <opencv2/core.hpp>
 #include <opencv2/core/base.hpp>
 #include <opencv2/core/matx.hpp>
+#include <opencv2/core/types.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
@@ -76,16 +77,135 @@ cv::Matx33d const Rb(
   0.00511951,  0.99982578,  0.01794987,
  -0.91193413,  0.01203307, -0.41016015);
 cv::Vec3d const tb = 0.5 * (tl + tr);
-cv::Vec3d normy(1, 0, 0);
+
+cv::Mat draw_arrows(const cv::Vec3d& vector, int imageSize, int lineThickness) {
+    // Create a blank canvas
+    int canvasWidth = imageSize * 3;
+    int canvasHeight = imageSize;
+    cv::Mat canvas = cv::Mat::zeros(canvasHeight, canvasWidth, CV_8UC3);
+
+    // Define colors for each projection
+    cv::Scalar colorXY(255, 0, 0); // Blue for XY
+    cv::Scalar colorXZ(0, 255, 0); // Green for XZ
+    cv::Scalar colorYZ(0, 0, 255); // Red for YZ
+
+    // Define the center points for drawing each projection
+    cv::Point centerXY(imageSize / 2, canvasHeight / 2);
+    cv::Point centerXZ(imageSize + imageSize / 2, canvasHeight / 2);
+    cv::Point centerYZ(2 * imageSize + imageSize / 2, canvasHeight / 2);
+
+    // Calculate the end points for each projection
+    auto const scale = 80.;
+    cv::Point endXY(centerXY.x + scale * vector[0], centerXY.y - scale * vector[1]);
+    cv::Point endXZ(centerXZ.x + scale * vector[0], centerXZ.y - scale * vector[2]);
+    cv::Point endYZ(centerYZ.x + scale * vector[1], centerYZ.y - scale * vector[2]);
+
+    // Draw the XY projection on the first part of the canvas
+    cv::line(canvas, centerXY, endXY, colorXY, lineThickness);
+    cv::putText(canvas, "XY", cv::Point(centerXY.x - 20, centerXY.y - 20), cv::FONT_HERSHEY_SIMPLEX, 0.5, colorXY, 1);
+
+    // Draw the XZ projection on the second part of the canvas
+    cv::line(canvas, centerXZ, endXZ, colorXZ, lineThickness);
+    cv::putText(canvas, "XZ", cv::Point(centerXZ.x - 20, centerXZ.y - 20), cv::FONT_HERSHEY_SIMPLEX, 0.5, colorXZ, 1);
+
+    // Draw the YZ projection on the third part of the canvas
+    cv::line(canvas, centerYZ, endYZ, colorYZ, lineThickness);
+    cv::putText(canvas, "YZ", cv::Point(centerYZ.x - 20, centerYZ.y - 20), cv::FONT_HERSHEY_SIMPLEX, 0.5, colorYZ, 1);
+
+    return canvas;
+}
+
+void mosaic(cv::Mat const& left, cv::Mat const& right, cv::Mat& warped_left, cv::Mat& warped_right);
+
+void refresh_mosaic() {
+  cv::Mat image1 = imgs.at(0);
+  cv::Mat image2 = imgs.at(1);
+  cv::Mat warpedImage1, warpedImage2, result;
+  mosaic(image2, image1, warpedImage1, warpedImage2);
+  cv::addWeighted(warpedImage1, 0.5, warpedImage2, 0.5, 0., result);
+  cv::resizeWindow("mosaic", result.cols / 2, result.rows / 2);
+  cv::imshow("mosaic", result);
+}
+// The range of the elements of normy can be [-1:1:0.1], default (0, 0, 1)
+// but the whole vector will stay normalized.
+// So as you pull on one slider, the other sliders will update
+cv::Vec3d normy(-0.5, 0, 0.);
+int nx_slider = -50;
+int const nx_max = 100;
+int ny_slider = 0;
+int const ny_max = 100;
+int nz_slider = 0;
+int const nz_max = 100;
+
+void refresh_control() {
+  std::cout << "normy = " << normy << "\n";
+  normy = cv::normalize(normy);
+  std::cout << "normy = " << normy << "\n";
+  std::cout << "normy.x trackbar pos = " << static_cast<int>(normy[0] * nx_max) << "\n";
+  std::cout << "normy.y trackbar pos = " << static_cast<int>(normy[1] * ny_max) << "\n";
+  std::cout << "normy.z trackbar pos = " << static_cast<int>(normy[2] * nz_max) << "\n";
+  cv::setTrackbarPos("nx", "control", static_cast<int>(normy[0] * nx_max)); 
+  cv::setTrackbarPos("ny", "control", static_cast<int>(normy[1] * ny_max)); 
+  cv::setTrackbarPos("nz", "control", static_cast<int>(normy[2] * nz_max)); 
+  cv::Mat normal_arrow = draw_arrows(normy, 200, 2);
+  cv::imshow("control", normal_arrow);
+}
+
+void on_nx(int, void*) {
+  normy[0] = static_cast<double>(nx_slider) / nx_max;
+  refresh_control();
+  refresh_mosaic();
+}
+
+void on_ny(int, void*) {
+  normy[1] = static_cast<double>(ny_slider) / ny_max;
+  refresh_control();
+  refresh_mosaic();
+}
+void on_nz(int, void*) {
+  normy[2] = static_cast<double>(nz_slider) / nz_max;
+  refresh_control();
+  refresh_mosaic();
+}
+
+// btl = [0.28, -0.03, 0.02]
+// btr = [-0.28, 0.03, 0.02]
+// normal = [0, 0, 1]
+//
+//btl = [0.28, -0.44, 0.02]
+// btr = [-0.28, 0.44, 0.02]
+// normal = [0, 0, 1]
+
+// We want the range of x to be [-2:2:0.1], default 0
+double x = 0.;
+int x_slider = 28;
+int const x_max = 100;
+
+void on_x(int, void*) {
+  x = static_cast<double>(x_slider) / x_max;
+  refresh_mosaic();
+}
+
+// We want the range of y to be [-2:2:0.1], default 0
+double y = 0.;
+int y_slider = 41;
+int const y_max = 100;
+
+void on_y(int, void*) {
+  y = static_cast<double>(y_slider) / y_max;
+  refresh_mosaic();
+}
+
+// We want the range of gdistance to be [0.1:10:0.1], default 1
 double gdistance = 10.;
 int d_slider = 10;
 int const d_max = 100;
-double x = 0.;
-int x_slider = 0;
-int const x_max = 100;
-double y = 0.;
-int y_slider = 0;
-int const y_max = 100;
+
+void on_d(int, void*) {
+  gdistance = static_cast<double>(d_max) / d_slider;
+  refresh_mosaic();
+}
+
 /**
  * Load images 
  * Define extrinsics and intrinsics 
@@ -159,8 +279,8 @@ void mosaic(cv::Mat const& left, cv::Mat const& right, cv::Mat& warped_left, cv:
   // btr = cv::Vec3d(-0.6, 0.73, 0.02); // manually tuned
   btr = cv::Vec3d(-0. + x, 0.03 + y, 0.02); // original 
   std::cout << "btr = " << btr << "\n";
-  cv::Vec3d normal = Rb * cv::Vec3d(0, 0, 1);
-  // cv::Vec3d normal = normy; 
+  // cv::Vec3d normal = Rb * cv::Vec3d(0, 0, 1);
+  cv::Vec3d normal = normy; 
   // normal[1] = 0.;
   normal = cv::normalize(normal);
   std::cout << "normal = " << normal << "\n";
@@ -178,36 +298,6 @@ void mosaic(cv::Mat const& left, cv::Mat const& right, cv::Mat& warped_left, cv:
   cv::warpPerspective(right, warped_right, homography_right, cv::Size(right.cols + 1000, right.rows + 4000));
 }
 
-void on_x(int, void*) {
-  x = static_cast<double>(x_slider) / x_max;
-  cv::Mat image1 = imgs.at(0);
-  cv::Mat image2 = imgs.at(1);
-  cv::Mat warpedImage1, warpedImage2, result;
-  mosaic(image2, image1, warpedImage1, warpedImage2);
-  cv::addWeighted(warpedImage1, 0.5, warpedImage2, 0.5, 0., result);
-  cv::resizeWindow("mosaic", result.cols / 2, result.rows / 2);
-  cv::imshow("mosaic", result);
-}
-void on_y(int, void*) {
-  y = static_cast<double>(y_slider) / y_max;
-  cv::Mat image1 = imgs.at(0);
-  cv::Mat image2 = imgs.at(1);
-  cv::Mat warpedImage1, warpedImage2, result;
-  mosaic(image2, image1, warpedImage1, warpedImage2);
-  cv::addWeighted(warpedImage1, 0.5, warpedImage2, 0.5, 0., result);
-  cv::resizeWindow("mosaic", result.cols / 2, result.rows / 2);
-  cv::imshow("mosaic", result);
-}
-void on_d(int, void*) {
-  gdistance = static_cast<double>(d_max) / d_slider;
-  cv::Mat image1 = imgs.at(0);
-  cv::Mat image2 = imgs.at(1);
-  cv::Mat warpedImage1, warpedImage2, result;
-  mosaic(image2, image1, warpedImage1, warpedImage2);
-  cv::addWeighted(warpedImage1, 0.5, warpedImage2, 0.5, 0., result);
-  cv::resizeWindow("mosaic", result.cols / 2, result.rows / 2);
-  cv::imshow("mosaic", result);
-}
 
 int main(int argc, char* argv[])
 {
@@ -219,17 +309,24 @@ int main(int argc, char* argv[])
     cv::Mat warpedImage1, warpedImage2, result;
     mosaic(image2, image1, warpedImage1, warpedImage2);
     cv::namedWindow("mosaic", cv::WINDOW_NORMAL);
-    cv::createTrackbar("x", "mosaic", &x_slider, x_max, on_x);
-    cv::setTrackbarMin("x", "mosaic", -100);
-    cv::createTrackbar("y", "mosaic", &y_slider, y_max, on_y);
-    cv::setTrackbarMin("y", "mosaic", -100);
-    cv::createTrackbar("d", "mosaic", &d_slider, d_max, on_d);
-    cv::setTrackbarMin("d", "mosaic", -100);
-    // cv::imwrite("image1.jpg", warpedImage1);
-    // cv::imwrite("image2.jpg", warpedImage2);
+    cv::namedWindow("control", cv::WINDOW_NORMAL);
+    cv::createTrackbar("x", "control", &x_slider, x_max, on_x);
+    cv::setTrackbarMin("x", "control", -100);
+    cv::createTrackbar("y", "control", &y_slider, y_max, on_y);
+    cv::setTrackbarMin("y", "control", -100);
+    cv::createTrackbar("d", "control", &d_slider, d_max, on_d);
+    cv::setTrackbarMin("d", "control", -100);
+    cv::createTrackbar("nx", "control", &nx_slider, nx_max, on_nx);
+    cv::setTrackbarMin("nx", "control", -100);
+    cv::createTrackbar("ny", "control", &ny_slider, ny_max, on_ny);
+    cv::setTrackbarMin("ny", "control", -100);
+    cv::createTrackbar("nz", "control", &nz_slider, nz_max, on_nz);
+    cv::setTrackbarMin("nz", "control", -100);
     cv::addWeighted(warpedImage1, 0.5, warpedImage2, 0.5, 0., result);
     cv::resizeWindow("mosaic", result.cols / 2, result.rows / 2);
     cv::imshow("mosaic", result);
+    cv::Mat normal_arrow = draw_arrows(normy, 200, 2);
+    cv::imshow("control", normal_arrow);
     cv::waitKey(0);
     // cv::imwrite("result.jpg", result);
     return EXIT_SUCCESS;
