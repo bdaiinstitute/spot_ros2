b7685b0e6a4650f933f784446830fd82d761c51b
Katie Hughes <khughes@theaiinstitute.com>
2024-07-03 12:11:17 -0400

    More framework based off the ros2 control demos

---
+++ spot_ros2_control/hardware/include/spot_ros2_control/spot.hpp
@@ -0,0 +1,73 @@
+// Copyright 2020 ros2_control Development Team
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef SPOT_ROS2_CONTROL__SPOT_HPP_
+#define SPOT_ROS2_CONTROL__SPOT_HPP_
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "hardware_interface/handle.hpp"
+#include "hardware_interface/hardware_info.hpp"
+#include "hardware_interface/system_interface.hpp"
+#include "hardware_interface/types/hardware_interface_return_values.hpp"
+#include "rclcpp/macros.hpp"
+#include "rclcpp_lifecycle/node_interfaces/lifecycle_node_interface.hpp"
+#include "rclcpp_lifecycle/state.hpp"
+#include "spot_ros2_control/visibility_control.h"
+
+namespace spot_ros2_control {
+class SpotMockJointHardwareInterface : public hardware_interface::SystemInterface {
+ public:
+  RCLCPP_SHARED_PTR_DEFINITIONS(SpotMockJointHardwareInterface);
+
+  SPOT_ROS2_CONTROL_PUBLIC
+  hardware_interface::CallbackReturn on_init(const hardware_interface::HardwareInfo& info) override;
+
+  SPOT_ROS2_CONTROL_PUBLIC
+  hardware_interface::CallbackReturn on_configure(const rclcpp_lifecycle::State& previous_state) override;
+
+  SPOT_ROS2_CONTROL_PUBLIC
+  std::vector<hardware_interface::StateInterface> export_state_interfaces() override;
+
+  SPOT_ROS2_CONTROL_PUBLIC
+  std::vector<hardware_interface::CommandInterface> export_command_interfaces() override;
+
+  SPOT_ROS2_CONTROL_PUBLIC
+  hardware_interface::CallbackReturn on_activate(const rclcpp_lifecycle::State& previous_state) override;
+
+  SPOT_ROS2_CONTROL_PUBLIC
+  hardware_interface::CallbackReturn on_deactivate(const rclcpp_lifecycle::State& previous_state) override;
+
+  SPOT_ROS2_CONTROL_PUBLIC
+  hardware_interface::return_type read(const rclcpp::Time& time, const rclcpp::Duration& period) override;
+
+  SPOT_ROS2_CONTROL_PUBLIC
+  hardware_interface::return_type write(const rclcpp::Time& time, const rclcpp::Duration& period) override;
+
+ private:
+  // Parameters for the RRBot simulation
+  double hw_start_sec_;
+  double hw_stop_sec_;
+  double hw_slowdown_;
+
+  // Store the command for the simulated robot
+  std::vector<double> hw_commands_;
+  std::vector<double> hw_states_;
+};
+
+}  // namespace spot_ros2_control
+
+#endif  // SPOT_ROS2_CONTROL__SPOT_HPP_
