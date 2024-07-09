3b4e2109da7c7d6eb07754f5baeef630306a4b41
Katie Hughes <khughes@theaiinstitute.com>
2024-07-09 11:11:20 -0400

    Add ros2 control packages to dependencies for installation

--- install_spot_ros2.sh
+++ install_spot_ros2.sh
@@ -25,7 +25,7 @@ sudo apt-get update
 
 # Install ROS dependencies
 # TODO(jschornak-bdai): use rosdep to install these packages by parsing dependencies listed in package.xml
-sudo apt install -y ros-$ROS_DISTRO-joint-state-publisher-gui ros-$ROS_DISTRO-tf-transformations ros-$ROS_DISTRO-xacro ros-$ROS_DISTRO-depth-image-proc ros-$ROS_DISTRO-tl-expected
+sudo apt install -y ros-$ROS_DISTRO-joint-state-publisher-gui ros-$ROS_DISTRO-tf-transformations ros-$ROS_DISTRO-xacro ros-$ROS_DISTRO-depth-image-proc ros-$ROS_DISTRO-tl-expected ros-$ROS_DISTRO-ros2-control ros-$ROS_DISTRO-ros2-controllers \
 # Install the dist-utils
 sudo apt-get install -y python3-distutils
 sudo apt-get install -y python3-apt
