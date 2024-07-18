504ea6818ff40a83d0ea30f51238e7f6ac12ed8d
Katie Hughes <157421702+khughes-bdai@users.noreply.github.com>
2024-07-18 15:54:29 -0400

    [SW-1175] ros2_control framework with mock hardware interface (#426)
    ## Change Overview
    
    Adds a framework for working on low level control of spot with ros2 control. 
    
    Currently, this adds a mock hardware interface to Spot's URDF and a forward position controller. This includes main launchfile `ros2 launch spot_ros2_control spot_ros2_control.launch.py` to bring up the ros2 control nodes for Spot. 
    
    Additionally, there are two nodes that use the forward position controller to move Spot's joints based off the Boston Dynamics SDK examples `noarm_squat` and `wiggle_arm` (https://github.com/boston-dynamics/spot-cpp-sdk/tree/master/cpp/examples/joint_control). They can be launched with `ros2 launch spot_ros2_control test_noarm_squat.launch.py` and `ros2 launch spot_ros2_control test_wiggle_arm.launch.py`. These are mainly meant as a proof of concept to show that the entire stack is functional. 
    
    ## Testing Done
    
    - [x] Test launchfiles successfully load the desired hardware interface and controller, and are able to move the joints 
    - [x] successfully builds and runs both inside and outside our internal docker

---
+++ spot_ros2_control/launch/spot_ros2_control.launch.py
@@ -0,0 +1,106 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import (
+    Command,
+    FindExecutable,
+    LaunchConfiguration,
+    PathJoinSubstitution,
+)
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    # Declare arguments
+    has_arm_arg = DeclareLaunchArgument(
+        "has_arm",
+        default_value="false",
+        choices=["true", "false"],
+        description="Whether the robot has an arm",
+    )
+    controllers_config_arg = DeclareLaunchArgument(
+        "controllers_config",
+        default_value="spot_controllers_without_arm.yaml",
+        description="YAML file for configuring the controllers.",
+    )
+    description_file_arg = DeclareLaunchArgument(
+        "description_file",
+        default_value="spot.urdf.xacro",
+        description="URDF/XACRO description file with the robot.",
+    )
+    robot_controller_arg = DeclareLaunchArgument(
+        "robot_controller",
+        default_value="forward_position_controller",
+        # This must match the controllers_config file. Right now this only has one option.
+        choices=["forward_position_controller"],
+        description="Robot controller to start.",
+    )
+
+    # Generate the robot description
+    robot_urdf = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution(
+                [FindPackageShare("spot_ros2_control"), "xacro", LaunchConfiguration("description_file")]
+            ),
+            " has_arm:=",
+            LaunchConfiguration("has_arm"),
+        ]
+    )
+    robot_description = {"robot_description": robot_urdf}
+
+    # Configuration files
+    controller_config_file = PathJoinSubstitution(
+        [FindPackageShare("spot_ros2_control"), "config", LaunchConfiguration("controllers_config")]
+    )
+    rviz_config_file = PathJoinSubstitution([FindPackageShare("spot_ros2_control"), "rviz", "spot_ros2_control.rviz"])
+
+    # Nodes
+    ros2_control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        output="both",
+        parameters=[robot_description, controller_config_file],
+    )
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="both",
+        parameters=[robot_description],
+    )
+    joint_state_broadcaster_spawner_node = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[
+            "joint_state_broadcaster",
+            "--controller-manager",
+            "/controller_manager",
+        ],
+    )
+    robot_controller_spawner_node = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[LaunchConfiguration("robot_controller"), "-c", "/controller_manager"],
+    )
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="log",
+        arguments=["-d", rviz_config_file],
+    )
+
+    return LaunchDescription(
+        [
+            has_arm_arg,
+            controllers_config_arg,
+            description_file_arg,
+            robot_controller_arg,
+            ros2_control_node,
+            robot_state_publisher_node,
+            joint_state_broadcaster_spawner_node,
+            robot_controller_spawner_node,
+            rviz_node,
+        ]
+    )
---
+++ spot_ros2_control/launch/test_noarm_squat.launch.py
@@ -0,0 +1,28 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    return LaunchDescription(
+        [
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(
+                    [
+                        PathJoinSubstitution(
+                            [FindPackageShare("spot_ros2_control"), "launch", "spot_ros2_control.launch.py"]
+                        )
+                    ]
+                ),
+                launch_arguments={
+                    "has_arm": "false",
+                    "controllers_config": "spot_controllers_without_arm.yaml",
+                    "robot_controller": "forward_position_controller",
+                }.items(),
+            ),
+            Node(package="spot_ros2_control", executable="noarm_squat", name="noarm_squat", output="screen"),
+        ]
+    )
