65c60fe1254861456ca983ce1924af728800fde2
Katie Hughes <157421702+khughes-bdai@users.noreply.github.com>
2024-07-25 15:34:08 -0400

    Update spot_hardware_interface.hpp

--- spot_ros2_control/hardware/include/spot_ros2_control/spot_hardware_interface.hpp
+++ spot_ros2_control/hardware/include/spot_ros2_control/spot_hardware_interface.hpp
@@ -95,7 +95,7 @@ class SpotHardware : public hardware_interface::SystemInterface {
  private:
   // TODO(khughes): Hard coding this for now, but there should be a cleaner way to do this.
   // The 3 interfaces are position, velocity, and effort.
-  size_t interfaces_per_joint_ = 3;
+  constexpr size_t interfaces_per_joint_ = 3;
 
   // Login info
   std::string hostname;
