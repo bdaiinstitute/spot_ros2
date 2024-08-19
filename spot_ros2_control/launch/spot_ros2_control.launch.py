ec19db3e0cc14af696ae7f65890295c069a965a4
Katie Hughes <157421702+khughes-bdai@users.noreply.github.com>
2024-08-19 16:33:06 -0400

    [SW-1316] ros2 control: auto determine if robot has an arm in launchfile (#454)
    ## Change Overview
    
    * when connected to a robot, the launchfile will auto determine if the robot has an arm and use that to generate the URDF that gets used by the hardware interface (same as in `spot_driver`)
        * default controller config file that gets loaded is also now auto determined from if the robot has an arm. 
        * This required some reworking of the core launchfile to use an opaque function to add this extra logic
    * Allows users to specify login info for the robot in a configuration yaml file, same as spot driver.
    * Introduces a dependency on `spot_driver` so that the launchfile can use this code in `spot_driver.launch.spot_launch_helpers`. This could be reworked in the future if we decide we don't want these packages so coupled.
    * minor changes to launch arguments to make the user interface simpler and more flexible, and updates readme
    
    ## Testing Done
    
    - [x] tested on robot with arm
    - [x] tested on robot without arm
    - [x] tested examples in mock mode with and without mock'ed arm
    
    Please create a checklist of tests you plan to do and check off the ones that have been completed successfully. Ensure that ROS 2 tests use `domain_coordinator` to prevent port conflicts. Further guidance for testing can be found on the [ros utilities wiki](https://github.com/bdaiinstitute/ros_utilities/wiki/Testing-guidelines).

--- spot_ros2_control/launch/spot_ros2_control.launch.py
+++ spot_ros2_control/launch/spot_ros2_control.launch.py
@@ -1,7 +1,8 @@
 # Copyright (c) 2024 Boston Dynamics AI Institute LLC. All rights reserved.
 
-from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch import LaunchContext, LaunchDescription
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
+from launch.conditions import IfCondition
 from launch.substitutions import (
     Command,
     FindExecutable,
@@ -11,108 +12,148 @@ from launch.substitutions import (
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
+from spot_driver.launch.spot_launch_helpers import get_login_parameters, spot_has_arm
 
-def generate_launch_description():
-    # Declare arguments
-    has_arm_arg = DeclareLaunchArgument(
-        "has_arm",
-        default_value="false",
-        choices=["true", "false"],
-        description="Whether the robot has an arm",
-    )
-    controllers_config_arg = DeclareLaunchArgument(
-        "controllers_config",
-        default_value="spot_controllers_without_arm.yaml",
-        description="YAML file for configuring the controllers.",
-    )
-    description_file_arg = DeclareLaunchArgument(
-        "description_file",
-        default_value="spot.urdf.xacro",
-        description="URDF/XACRO description file with the robot.",
-    )
-    robot_controller_arg = DeclareLaunchArgument(
-        "robot_controller",
-        default_value="forward_position_controller",
-        # This must match the controllers_config file. Right now this only has one option.
-        choices=["forward_position_controller"],
-        description="Robot controller to start.",
-    )
-    hardware_interface_arg = DeclareLaunchArgument(
-        "hardware_interface",
-        default_value="mock",
-        # Must match the xacro file options for which plugin to load
-        choices=["mock", "spot-sdk"],
-        description="Hardware interface to load",
-    )
 
-    # Generate the robot description
+def launch_setup(context: LaunchContext, ld: LaunchDescription) -> None:
+    hardware_interface: str = LaunchConfiguration("hardware_interface").perform(context)
+    controllers_config: str = LaunchConfiguration("controllers_config").perform(context)
+    mock_has_arm: bool = IfCondition(LaunchConfiguration("mock_has_arm")).evaluate(context)
+
+    # If connected to a physical robot, query if it has an arm. Otherwise, use the value in mock_has_arm.
+    if hardware_interface == "robot":
+        config_file = LaunchConfiguration("config_file").perform(context)
+        has_arm = spot_has_arm(config_file_path=config_file, spot_name="")
+        username, password, hostname = get_login_parameters(config_file)[:3]
+        login_params = f" hostname:={hostname} username:={username} password:={password}"
+    else:
+        has_arm = mock_has_arm
+        login_params = ""
+
+    # Generate the robot description based off if the robot has an arm.
     robot_urdf = Command(
         [
             PathJoinSubstitution([FindExecutable(name="xacro")]),
             " ",
-            PathJoinSubstitution(
-                [FindPackageShare("spot_ros2_control"), "xacro", LaunchConfiguration("description_file")]
-            ),
+            PathJoinSubstitution([FindPackageShare("spot_ros2_control"), "xacro", "spot.urdf.xacro"]),
             " has_arm:=",
-            LaunchConfiguration("has_arm"),
+            str(has_arm),
             " hardware_interface_type:=",
             LaunchConfiguration("hardware_interface"),
+            login_params,
         ]
     )
     robot_description = {"robot_description": robot_urdf}
 
-    # Configuration files
-    controller_config_file = PathJoinSubstitution(
-        [FindPackageShare("spot_ros2_control"), "config", LaunchConfiguration("controllers_config")]
-    )
-    rviz_config_file = PathJoinSubstitution([FindPackageShare("spot_ros2_control"), "rviz", "spot_ros2_control.rviz"])
+    # Configuration for the controller.
+    # If not controller is selected, use the appropriate default given if the robot has an arm or not.
+    # Else, just use the yaml that is passed in.
+    if controllers_config == "":
+        default_config_file = "spot_controllers_with_arm.yaml" if has_arm else "spot_controllers_without_arm.yaml"
+        controllers_config = PathJoinSubstitution(
+            [FindPackageShare("spot_ros2_control"), "config", default_config_file]
+        )
 
-    # Nodes
-    ros2_control_node = Node(
-        package="controller_manager",
-        executable="ros2_control_node",
-        output="both",
-        parameters=[robot_description, controller_config_file],
+    # Add nodes
+    ld.add_action(
+        Node(
+            package="controller_manager",
+            executable="ros2_control_node",
+            output="both",
+            parameters=[robot_description, controllers_config],
+        )
     )
-    robot_state_publisher_node = Node(
-        package="robot_state_publisher",
-        executable="robot_state_publisher",
-        output="both",
-        parameters=[robot_description],
+    ld.add_action(
+        Node(
+            package="robot_state_publisher",
+            executable="robot_state_publisher",
+            output="both",
+            parameters=[robot_description],
+        )
     )
-    joint_state_broadcaster_spawner_node = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=[
-            "joint_state_broadcaster",
-            "--controller-manager",
-            "/controller_manager",
-        ],
+    ld.add_action(
+        Node(
+            package="controller_manager",
+            executable="spawner",
+            arguments=[
+                "joint_state_broadcaster",
+                "--controller-manager",
+                "/controller_manager",
+            ],
+        )
     )
-    robot_controller_spawner_node = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=[LaunchConfiguration("robot_controller"), "-c", "/controller_manager"],
+    ld.add_action(
+        Node(
+            package="controller_manager",
+            executable="spawner",
+            arguments=[LaunchConfiguration("robot_controller"), "-c", "/controller_manager"],
+        )
     )
-    rviz_node = Node(
-        package="rviz2",
-        executable="rviz2",
-        name="rviz2",
-        output="log",
-        arguments=["-d", rviz_config_file],
+    ld.add_action(
+        Node(
+            package="rviz2",
+            executable="rviz2",
+            name="rviz2",
+            output="log",
+            arguments=[
+                "-d",
+                PathJoinSubstitution([FindPackageShare("spot_ros2_control"), "rviz", "spot_ros2_control.rviz"]),
+            ],
+            condition=IfCondition(LaunchConfiguration("launch_rviz")),
+        )
     )
+    return
 
-    return LaunchDescription(
+
+def generate_launch_description():
+    # Populate launch description with launch arguments
+    ld = LaunchDescription(
         [
-            has_arm_arg,
-            controllers_config_arg,
-            description_file_arg,
-            robot_controller_arg,
-            hardware_interface_arg,
-            ros2_control_node,
-            robot_state_publisher_node,
-            joint_state_broadcaster_spawner_node,
-            robot_controller_spawner_node,
-            rviz_node,
+            DeclareLaunchArgument(
+                "hardware_interface",
+                default_value="mock",
+                # Must match the xacro file options for which plugin to load
+                choices=["mock", "robot"],
+                description=(
+                    "Hardware interface to load. 'mock' loads a simple interface useful for testing that forwards"
+                    " commands directly to state. 'robot' uses a custom hardware interface using the Spot C++ SDK to"
+                    " connect to the physical robot."
+                ),
+            ),
+            DeclareLaunchArgument(
+                "config_file",
+                default_value="",
+                description="Path to general configuration file optionally containing login information.",
+            ),
+            DeclareLaunchArgument(
+                "controllers_config",
+                default_value="",
+                description=(
+                    "Configuration file for the controllers loaded. If not set, a default config file containing a"
+                    " forward position controller and a joint state publisher will be loaded, with the appropriate"
+                    " configuration based on whether or not the robot has an arm."
+                ),
+            ),
+            DeclareLaunchArgument(
+                "robot_controller",
+                default_value="forward_position_controller",
+                choices=["forward_position_controller"],
+                description="Robot controller to start. Must match an entry in controller_config.",
+            ),
+            DeclareLaunchArgument(
+                "mock_has_arm",
+                default_value="false",
+                choices=["True", "true", "False", "false"],
+                description="If in hardware_interface:=mock mode, whether or not the mocked robot has an arm.",
+            ),
+            DeclareLaunchArgument(
+                "launch_rviz",
+                default_value="True",
+                choices=["True", "true", "False", "false"],
+                description="Flag to enable rviz.",
+            ),
         ]
     )
+    # Add nodes to launch description
+    ld.add_action(OpaqueFunction(function=launch_setup, args=[ld]))
+    return ld
