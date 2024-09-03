d337744b7ca5682906a9f76a840a428fcae7ab6c
Laura Lee <llee@theaiinstitute.com>
2024-09-03 23:30:22 +0000

    Add joint command passthrough example

--- spot_ros2_control/hardware/include/spot_ros2_control/spot_joint_map.hpp
+++ spot_ros2_control/hardware/include/spot_ros2_control/spot_joint_map.hpp
@@ -68,45 +68,65 @@ static const std::vector<float> arm_kd = {5.20, 5.20, 2.04, 5.20, 5.20, 2.04, 5.
 /// @param msg JointStates message
 /// @param ordered_joint_angles_ Joint positions from the joint state message following the correct order.
 /// @return boolean indicating if the joint angles got ordered successfully.
-bool order_joints(const sensor_msgs::msg::JointState& msg, std::vector<double>& ordered_joint_angles) {
-  const auto njoints = msg.position.size();
-  ordered_joint_angles.resize(njoints);
-  // Different joint index maps for arm-full and arm-less
-  switch (njoints) {
+bool order_joints(const sensor_msgs::msg::JointState &msg, std::vector<double> &ordered_joint_angles) {
+    const auto njoints = msg.position.size();
+    ordered_joint_angles.resize(njoints);
+    // Different joint index maps for arm-full and arm-less
+    switch (njoints) {
     // case without arm
     case kNjointsNoArm:
-      for (size_t i = 0; i < njoints; i++) {
-        // get the joint name
-        const auto joint_name = msg.name.at(i);
-        try {
-          const auto joint_index = kJointNameToIndexWithoutArm.at(joint_name);
-          ordered_joint_angles.at(joint_index) = msg.position.at(i);
-        } catch (const std::out_of_range& e) {
-          RCLCPP_INFO_STREAM(rclcpp::get_logger("SpotJointMap"), "Invalid joint: " << joint_name);
-          return false;
+        for (size_t i = 0; i < njoints; i++) {
+            // get the joint name
+            const auto joint_name = msg.name.at(i);
+            try {
+                const auto joint_index = kJointNameToIndexWithoutArm.at(joint_name);
+                ordered_joint_angles.at(joint_index) = msg.position.at(i);
+            } catch (const std::out_of_range &e) {
+                RCLCPP_INFO_STREAM(rclcpp::get_logger("SpotJointMap"), "Invalid joint: " << joint_name);
+                return false;
+            }
         }
-      }
-      return true;
+        return true;
 
     // case with arm
     case kNjointsArm:
-      for (size_t i = 0; i < njoints; i++) {
-        // get the joint name
-        const auto joint_name = msg.name.at(i);
-        try {
-          const auto joint_index = kJointNameToIndexWithArm.at(joint_name);
-          ordered_joint_angles.at(joint_index) = msg.position.at(i);
-        } catch (const std::out_of_range& e) {
-          RCLCPP_INFO_STREAM(rclcpp::get_logger("SpotHardware"), "Invalid joint: " << joint_name);
-          return false;
+        for (size_t i = 0; i < njoints; i++) {
+            // get the joint name
+            const auto joint_name = msg.name.at(i);
+            try {
+                const auto joint_index = kJointNameToIndexWithArm.at(joint_name);
+                ordered_joint_angles.at(joint_index) = msg.position.at(i);
+            } catch (const std::out_of_range &e) {
+                RCLCPP_INFO_STREAM(rclcpp::get_logger("SpotHardware"), "Invalid joint: " << joint_name);
+                return false;
+            }
         }
-      }
-      return true;
+        return true;
 
     default:
-      RCLCPP_INFO_STREAM(rclcpp::get_logger("SpotHardware"), "Invalid number of joints: " << njoints);
-      return false;
-  }
+        RCLCPP_INFO_STREAM(rclcpp::get_logger("SpotHardware"), "Invalid number of joints: " << njoints);
+        return false;
+    }
 }
 
-}  // namespace spot_ros2_control
+/// @brief Given a joint name (possibly with namespace), return the joint index
+/// @param joint_str string name of joint
+/// @param has_arm whether or not the spot has an arm (default true)
+/// @return int joint index
+int get_joint_index(const std::string &joint_str, bool has_arm = true) {
+    // Check if the joint_str has a namespace - if so, remove it
+    size_t namespace_pos = joint_str.find("/");
+    std::string joint_name = (namespace_pos != std::string::npos) ? joint_str.substr(namespace_pos + 1) : joint_str;
+
+    if (kJointNameToIndexWithArm.find(joint_name) == kJointNameToIndexWithArm.end() &&
+        kJointNameToIndexWithoutArm.find(joint_name) == kJointNameToIndexWithoutArm.end()) {
+        RCLCPP_ERROR(rclcpp::get_logger("SpotHardware"), "Cannot find joint %s in joint map.",
+                           joint_name.c_str());
+        return -1;
+    }
+    int joint_idx = has_arm ? kJointNameToIndexWithArm.at(joint_name) : kJointNameToIndexWithoutArm.at(joint_name);
+
+    return joint_idx;
+}
+
+} // namespace spot_ros2_control
