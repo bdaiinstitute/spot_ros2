2a88854a8d790fce8ea6ae6d21ce67972d14bef6
Michel Hidalgo <mhidalgo@theaiinstitute.com>
2025-02-26 15:29:07 -0300

    Complete lease manager API
    - Add claim/release services
    - Include periodic lease status
    
    Signed-off-by: Michel Hidalgo <mhidalgo@theaiinstitute.com>

--- spot_driver/include/spot_driver/lease/lease_manager.hpp
+++ spot_driver/include/spot_driver/lease/lease_manager.hpp
@@ -4,19 +4,25 @@
 
 #include <spot_driver/api/lease_client_interface.hpp>
 #include <spot_driver/interfaces/logger_interface_base.hpp>
+#include <spot_driver/interfaces/timer_interface_base.hpp>
 
+#include <spot_msgs/msg/lease_array.hpp>
 #include <spot_msgs/srv/acquire_lease.hpp>
 #include <spot_msgs/srv/return_lease.hpp>
+#include <std_srvs/srv/trigger.hpp>
 
 #include <memory>
 #include <mutex>
+#include <optional>
 #include <string>
 #include <unordered_map>
 
 namespace spot_ros2::lease {
 
+using spot_msgs::msg::LeaseArray;
 using spot_msgs::srv::AcquireLease;
 using spot_msgs::srv::ReturnLease;
+using std_srvs::srv::Trigger;
 
 /**
  * Lease management for a ROS 2 graph.
@@ -39,6 +45,10 @@ class LeaseManager {
       virtual ~Bond() = default;
     };
 
+    virtual void createClaimLeasesService(
+        const std::string& service_name,
+        std::function<void(const std::shared_ptr<Trigger::Request>, std::shared_ptr<Trigger::Response>)> callback) = 0;
+
     virtual void createAcquireLeaseService(
         const std::string& service_name,
         std::function<void(const std::shared_ptr<AcquireLease::Request>, std::shared_ptr<AcquireLease::Response>)>
@@ -49,7 +59,13 @@ class LeaseManager {
         std::function<void(const std::shared_ptr<ReturnLease::Request>, std::shared_ptr<ReturnLease::Response>)>
             callback) = 0;
 
+    virtual void createReleaseLeasesService(
+        const std::string& service_name,
+        std::function<void(const std::shared_ptr<Trigger::Request>, std::shared_ptr<Trigger::Response>)> callback) = 0;
+
     virtual std::shared_ptr<Bond> createBond(const std::string& node_name, std::function<void()> break_callback) = 0;
+
+    virtual void publishLeaseArray(std::unique_ptr<LeaseArray> message) = 0;
 
     virtual ~MiddlewareHandle() = default;
   };
@@ -57,14 +73,31 @@ class LeaseManager {
   /**
    * Instantiate the lease manager.
    * @param lease_client The ROS node.
-   * @param logger Logging interface.
-   * @param middleware_handle
+   * @param logger_interface Logging interface.
+   * @param timer_interface Timers interface.
+   * @param middleware_handle Middleware interfaces' provider.
+   * @param lease_check_rate Optional rate, in Hz, to check and report on robot leasing status.
    */
-  explicit LeaseManager(std::shared_ptr<LeaseClientInterface> lease_client, std::shared_ptr<LoggerInterfaceBase> logger,
-                        std::unique_ptr<MiddlewareHandle> middleware_handle);
+  explicit LeaseManager(std::shared_ptr<LeaseClientInterface> lease_client,
+                        std::unique_ptr<LoggerInterfaceBase> logger_interface,
+                        std::unique_ptr<TimerInterfaceBase> timer_interface,
+                        std::unique_ptr<MiddlewareHandle> middleware_handle, std::optional<double> lease_check_rate);
 
-  /** Initialize the service. */
+  /**
+   * Initialize the lease manager.
+   */
   void initialize();
+
+  /**
+   * Claim leases on request.
+   *
+   * This is achieved by acquiring the root resource lease.
+   * Typically used to implement the associated service.
+   *
+   * @param request The ROS request.
+   * @param response A ROS response to be filled.
+   */
+  void claimLeases(const std::shared_ptr<Trigger::Request> request, std::shared_ptr<Trigger::Response> response);
 
   /**
    * Acquire lease on request.
@@ -88,7 +121,22 @@ class LeaseManager {
   void returnLease(const std::shared_ptr<ReturnLease::Request> request,
                    std::shared_ptr<ReturnLease::Response> response);
 
+  /**
+   * Release all acquired leases on request.
+   *
+   * Typically used to implement the associated service.
+   *
+   * @param request The ROS request.
+   * @param response A ROS response to be filled.
+   */
+  void releaseLeases(const std::shared_ptr<Trigger::Request> request, std::shared_ptr<Trigger::Response> response);
+
  private:
+  /**
+   * Check (and report) leasing status.
+   */
+  void checkLeasingStatus();
+
   /**
    * Handle lease retention failure.
    *
@@ -102,10 +150,16 @@ class LeaseManager {
   std::shared_ptr<LeaseClientInterface> lease_client_;
 
   // An interface to logging API.
-  std::shared_ptr<LoggerInterfaceBase> logger_;
+  std::unique_ptr<LoggerInterfaceBase> logger_interface_;
+
+  // An interface to the timers API.
+  std::unique_ptr<TimerInterfaceBase> timer_interface_;
 
   // An interface to the middleware provider.
   std::unique_ptr<MiddlewareHandle> middleware_handle_;
+
+  // Optional lease check rate.
+  std::optional<double> lease_check_rate_;
 
   struct ManagedSublease {
     bosdyn::client::Lease lease;                   // Actual SDK sublease.
@@ -114,7 +168,7 @@ class LeaseManager {
   // Storage for active subleases (and associated keepalives).
   std::unordered_map<std::string, ManagedSublease> subleases_;
 
-  // Synchronization primitive for sublease storage.
-  std::recursive_mutex subleases_mutex_;
+  // Synchronization primitive.
+  std::recursive_mutex mutex_;
 };
 }  // namespace spot_ros2::lease
--- spot_driver/src/lease/lease_middleware_handle.cpp
+++ spot_driver/src/lease/lease_middleware_handle.cpp
@@ -6,7 +6,15 @@
 
 namespace spot_ros2::lease {
 
-LeaseMiddlewareHandle::LeaseMiddlewareHandle(std::shared_ptr<rclcpp::Node> node) : node_{node} {}
+LeaseMiddlewareHandle::LeaseMiddlewareHandle(std::shared_ptr<rclcpp::Node> node) : node_{node} {
+  lease_array_publisher_ = node_->create_publisher<LeaseArray>("status/leases", 1);
+}
+
+void LeaseMiddlewareHandle::createClaimLeasesService(
+    const std::string& service_name,
+    std::function<void(const std::shared_ptr<Trigger::Request>, std::shared_ptr<Trigger::Response>)> callback) {
+  claim_leases_service_ = node_->create_service<Trigger>(service_name, callback);
+}
 
 void LeaseMiddlewareHandle::createAcquireLeaseService(
     const std::string& service_name,
@@ -19,6 +27,12 @@ void LeaseMiddlewareHandle::createReturnLeaseService(
     const std::string& service_name,
     std::function<void(const std::shared_ptr<ReturnLease::Request>, std::shared_ptr<ReturnLease::Response>)> callback) {
   return_lease_service_ = node_->create_service<ReturnLease>(service_name, callback);
+}
+
+void LeaseMiddlewareHandle::createReleaseLeasesService(
+    const std::string& service_name,
+    std::function<void(const std::shared_ptr<Trigger::Request>, std::shared_ptr<Trigger::Response>)> callback) {
+  release_leases_service_ = node_->create_service<Trigger>(service_name, callback);
 }
 
 namespace {
@@ -39,6 +53,10 @@ class BondHolder : public LeaseManager::MiddlewareHandle::Bond {
 std::shared_ptr<LeaseManager::MiddlewareHandle::Bond> LeaseMiddlewareHandle::createBond(
     const std::string& node_name, std::function<void()> break_callback) {
   return std::make_shared<BondHolder>(node_, node_name, std::move(break_callback));
+}
+
+void LeaseMiddlewareHandle::publishLeaseArray(std::unique_ptr<LeaseArray> message) {
+  lease_array_publisher_->publish(std::move(message));
 }
 
 }  // namespace spot_ros2::lease
