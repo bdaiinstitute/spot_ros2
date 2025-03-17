9c799cdab2aef3c136890a6f1ece36f52facb534
Katie Hughes <khughes-bdai@theaiinstitute.com>
2025-03-17 17:02:24 -0400

    Builds
    Signed-off-by: Katie Hughes <khughes-bdai@theaiinstitute.com>

--- spot_controllers/src/foot_state_broadcaster.cpp
+++ spot_controllers/src/foot_state_broadcaster.cpp
@@ -57,80 +57,32 @@ controller_interface::InterfaceConfiguration FootStateBroadcaster::command_inter
 controller_interface::InterfaceConfiguration FootStateBroadcaster::state_interface_configuration() const {
   controller_interface::InterfaceConfiguration state_interfaces_config;
 
-  if (use_all_available_interfaces()) {
-    state_interfaces_config.type = controller_interface::interface_configuration_type::ALL;
-  } else {
-    state_interfaces_config.type = controller_interface::interface_configuration_type::INDIVIDUAL;
-    for (const auto& joint : params_.joints) {
-      for (const auto& interface : params_.interfaces) {
-        state_interfaces_config.names.push_back(joint + "/" + interface);
-      }
-    }
-  }
-
+  state_interfaces_config.type = controller_interface::interface_configuration_type::INDIVIDUAL;
+  //   for (const auto& joint : params_.joints) {
+  //     for (const auto& interface : params_.interfaces) {
+  //       state_interfaces_config.names.push_back(joint + "/" + interface);
+  //     }
+  //   }
   return state_interfaces_config;
 }
 
 controller_interface::CallbackReturn FootStateBroadcaster::on_configure(
     const rclcpp_lifecycle::State& /*previous_state*/) {
   params_ = param_listener_->get_params();
-
-  if (use_all_available_interfaces()) {
-    RCLCPP_INFO(get_node()->get_logger(),
-                "'joints' or 'interfaces' parameter is empty. "
-                "All available state interfaces will be published");
-    params_.joints.clear();
-    params_.interfaces.clear();
-  } else {
-    RCLCPP_INFO(get_node()->get_logger(),
-                "Publishing state interfaces defined in 'joints' and 'interfaces' parameters.");
-  }
-
-  auto get_map_interface_parameter = [&](std::string const& interface, std::string const& interface_to_map) {
-    if (std::find(params_.interfaces.begin(), params_.interfaces.end(), interface) != params_.interfaces.end()) {
-      map_interface_to_joint_state_[interface] = interface;
-      RCLCPP_WARN(get_node()->get_logger(),
-                  "Mapping from '%s' to interface '%s' will not be done, because '%s' is defined "
-                  "in 'interface' parameter.",
-                  interface_to_map.c_str(), interface.c_str(), interface.c_str());
-    } else {
-      map_interface_to_joint_state_[interface_to_map] = interface;
-    }
-  };
-
-  map_interface_to_joint_state_ = {};
-  get_map_interface_parameter(HW_IF_POSITION, params_.map_interface_to_joint_state.position);
-  get_map_interface_parameter(HW_IF_VELOCITY, params_.map_interface_to_joint_state.velocity);
-  get_map_interface_parameter(HW_IF_EFFORT, params_.map_interface_to_joint_state.effort);
+  RCLCPP_INFO(get_node()->get_logger(), "CONFIGURING");
 
   try {
-    const std::string topic_name_prefix = params_.use_local_topics ? "~/" : "";
+    const std::string topic_name_prefix = "";
 
-    joint_state_publisher_ = get_node()->create_publisher<sensor_msgs::msg::JointState>(
-        topic_name_prefix + "joint_states", rclcpp::SystemDefaultsQoS());
+    foot_state_publisher_ = get_node()->create_publisher<spot_msgs::msg::FootStateArray>(
+        topic_name_prefix + "feet_states", rclcpp::SystemDefaultsQoS());
 
-    realtime_joint_state_publisher_ =
-        std::make_shared<realtime_tools::RealtimePublisher<sensor_msgs::msg::JointState>>(joint_state_publisher_);
-
-    dynamic_joint_state_publisher_ = get_node()->create_publisher<control_msgs::msg::DynamicJointState>(
-        topic_name_prefix + "dynamic_joint_states", rclcpp::SystemDefaultsQoS());
-
-    realtime_dynamic_joint_state_publisher_ =
-        std::make_shared<realtime_tools::RealtimePublisher<control_msgs::msg::DynamicJointState>>(
-            dynamic_joint_state_publisher_);
+    realtime_foot_state_publisher_ =
+        std::make_shared<realtime_tools::RealtimePublisher<spot_msgs::msg::FootStateArray>>(foot_state_publisher_);
   } catch (const std::exception& e) {
     // get_node() may throw, logging raw here
     fprintf(stderr, "Exception thrown during init stage with message: %s \n", e.what());
     return CallbackReturn::ERROR;
-  }
-
-  const std::string& urdf = get_robot_description();
-
-  is_model_loaded_ = !urdf.empty() && model_.initString(urdf);
-  if (!is_model_loaded_) {
-    RCLCPP_ERROR(get_node()->get_logger(),
-                 "Failed to parse robot description. Will publish all the interfaces with '%s', '%s' and '%s'",
-                 HW_IF_POSITION, HW_IF_VELOCITY, HW_IF_EFFORT);
   }
 
   return CallbackReturn::SUCCESS;
@@ -138,173 +90,47 @@ controller_interface::CallbackReturn FootStateBroadcaster::on_configure(
 
 controller_interface::CallbackReturn FootStateBroadcaster::on_activate(
     const rclcpp_lifecycle::State& /*previous_state*/) {
-  if (!init_joint_data()) {
-    RCLCPP_ERROR(get_node()->get_logger(), "Error initializing joint data. FootStateBroadcaster will not run.");
-    return CallbackReturn::ERROR;
+  // initialize foot state message
+  const size_t num_feet = 4;
+  // default initialization for foot state message
+  auto& feet_state_msg = realtime_foot_state_publisher_->msg_;
+  // update joint state message and dynamic joint state message
+  for (size_t i = 0; i < num_feet; ++i) {
+    spot_msgs::msg::FootState foot_state;
+    foot_state.contact = spot_msgs::msg::FootState::CONTACT_UNKNOWN;
+    feet_state_msg.states[i] = foot_state;
   }
-
-  init_joint_state_msg();
-  init_dynamic_joint_state_msg();
 
   return CallbackReturn::SUCCESS;
 }
 
 controller_interface::CallbackReturn FootStateBroadcaster::on_deactivate(
     const rclcpp_lifecycle::State& /*previous_state*/) {
-  joint_names_.clear();
-  name_if_value_mapping_.clear();
-
   return CallbackReturn::SUCCESS;
-}
-
-template <typename T>
-bool has_any_key(const std::unordered_map<std::string, T>& map, const std::vector<std::string>& keys) {
-  bool found_key = false;
-  for (const auto& key_item : map) {
-    const auto& key = key_item.first;
-    if (std::find(keys.cbegin(), keys.cend(), key) != keys.cend()) {
-      found_key = true;
-      break;
-    }
-  }
-  return found_key;
-}
-
-bool FootStateBroadcaster::init_joint_data() {
-  joint_names_.clear();
-  if (state_interfaces_.empty()) {
-    RCLCPP_ERROR(get_node()->get_logger(), "No state interfaces found to publish.");
-    return false;
-  }
-
-  // loop in reverse order, this maintains the order of values at retrieval time
-  for (auto si = state_interfaces_.crbegin(); si != state_interfaces_.crend(); si++) {
-    // initialize map if name is new
-    if (name_if_value_mapping_.count(si->get_prefix_name()) == 0) {
-      name_if_value_mapping_[si->get_prefix_name()] = {};
-    }
-    // add interface name
-    std::string interface_name = si->get_interface_name();
-    if (map_interface_to_joint_state_.count(interface_name) > 0) {
-      interface_name = map_interface_to_joint_state_[interface_name];
-    }
-    name_if_value_mapping_[si->get_prefix_name()][interface_name] = kUninitializedValue;
-  }
-
-  // filter state interfaces that have at least one of the joint_states fields,
-  // the rest will be ignored for this message
-  for (const auto& name_ifv : name_if_value_mapping_) {
-    const auto& interfaces_and_values = name_ifv.second;
-    if (has_any_key(interfaces_and_values, {HW_IF_POSITION, HW_IF_VELOCITY, HW_IF_EFFORT})) {
-      if (!params_.use_urdf_to_filter || !params_.joints.empty() || !is_model_loaded_ ||
-          model_.getJoint(name_ifv.first)) {
-        joint_names_.push_back(name_ifv.first);
-      }
-    }
-  }
-
-  // Add extra joints from parameters, each joint will be added to joint_names_ and
-  // name_if_value_mapping_ if it is not already there
-  rclcpp::Parameter extra_joints;
-  if (get_node()->get_parameter("extra_joints", extra_joints)) {
-    const std::vector<std::string>& extra_joints_names = extra_joints.as_string_array();
-    for (const auto& extra_joint_name : extra_joints_names) {
-      if (name_if_value_mapping_.count(extra_joint_name) == 0) {
-        name_if_value_mapping_[extra_joint_name] = {{HW_IF_POSITION, 0.0}, {HW_IF_VELOCITY, 0.0}, {HW_IF_EFFORT, 0.0}};
-        joint_names_.push_back(extra_joint_name);
-      }
-    }
-  }
-
-  return true;
-}
-
-void FootStateBroadcaster::init_joint_state_msg() {
-  const size_t num_joints = joint_names_.size();
-
-  /// @note joint_state_msg publishes position, velocity and effort for all joints,
-  /// with at least one of these interfaces, the rest are omitted from this message
-
-  // default initialization for joint state message
-  auto& joint_state_msg = realtime_joint_state_publisher_->msg_;
-  joint_state_msg.name = joint_names_;
-  joint_state_msg.position.resize(num_joints, kUninitializedValue);
-  joint_state_msg.velocity.resize(num_joints, kUninitializedValue);
-  joint_state_msg.effort.resize(num_joints, kUninitializedValue);
-}
-
-void FootStateBroadcaster::init_dynamic_joint_state_msg() {
-  auto& dynamic_joint_state_msg = realtime_dynamic_joint_state_publisher_->msg_;
-  dynamic_joint_state_msg.joint_names.clear();
-  dynamic_joint_state_msg.interface_values.clear();
-  for (const auto& name_ifv : name_if_value_mapping_) {
-    const auto& name = name_ifv.first;
-    const auto& interfaces_and_values = name_ifv.second;
-    dynamic_joint_state_msg.joint_names.push_back(name);
-    control_msgs::msg::InterfaceValue if_value;
-    for (const auto& interface_and_value : interfaces_and_values) {
-      if_value.interface_names.emplace_back(interface_and_value.first);
-      if_value.values.emplace_back(kUninitializedValue);
-    }
-    dynamic_joint_state_msg.interface_values.emplace_back(if_value);
-  }
-}
-
-bool FootStateBroadcaster::use_all_available_interfaces() const {
-  return params_.joints.empty() || params_.interfaces.empty();
-}
-
-double get_value(const std::unordered_map<std::string, std::unordered_map<std::string, double>>& map,
-                 const std::string& name, const std::string& interface_name) {
-  const auto& interfaces_and_values = map.at(name);
-  const auto interface_and_value = interfaces_and_values.find(interface_name);
-  if (interface_and_value != interfaces_and_values.cend()) {
-    return interface_and_value->second;
-  } else {
-    return kUninitializedValue;
-  }
 }
 
 controller_interface::return_type FootStateBroadcaster::update(const rclcpp::Time& time,
                                                                const rclcpp::Duration& /*period*/) {
-  for (const auto& state_interface : state_interfaces_) {
-    std::string interface_name = state_interface.get_interface_name();
-    if (map_interface_to_joint_state_.count(interface_name) > 0) {
-      interface_name = map_interface_to_joint_state_[interface_name];
-    }
-    name_if_value_mapping_[state_interface.get_prefix_name()][interface_name] = state_interface.get_value();
-    RCLCPP_DEBUG(get_node()->get_logger(), "%s: %f\n", state_interface.get_name().c_str(), state_interface.get_value());
-  }
+  //   for (const auto& state_interface : state_interfaces_) {
+  //     std::string interface_name = state_interface.get_interface_name();
+  //     if (map_interface_to_joint_state_.count(interface_name) > 0) {
+  //       interface_name = map_interface_to_joint_state_[interface_name];
+  //     }
+  //     name_if_value_mapping_[state_interface.get_prefix_name()][interface_name] = state_interface.get_value();
+  //     RCLCPP_DEBUG(get_node()->get_logger(), "%s: %f\n", state_interface.get_name().c_str(),
+  //     state_interface.get_value());
+  //   }
 
-  if (realtime_joint_state_publisher_ && realtime_joint_state_publisher_->trylock()) {
-    auto& joint_state_msg = realtime_joint_state_publisher_->msg_;
-
-    joint_state_msg.header.stamp = time;
+  if (realtime_foot_state_publisher_ && realtime_foot_state_publisher_->trylock()) {
+    auto& feet_state_msg = realtime_foot_state_publisher_->msg_;
 
     // update joint state message and dynamic joint state message
-    for (size_t i = 0; i < joint_names_.size(); ++i) {
-      joint_state_msg.position[i] = get_value(name_if_value_mapping_, joint_names_[i], HW_IF_POSITION);
-      joint_state_msg.velocity[i] = get_value(name_if_value_mapping_, joint_names_[i], HW_IF_VELOCITY);
-      joint_state_msg.effort[i] = get_value(name_if_value_mapping_, joint_names_[i], HW_IF_EFFORT);
+    for (size_t i = 0; i < 4; ++i) {
+      spot_msgs::msg::FootState foot_state;
+      foot_state.contact = spot_msgs::msg::FootState::CONTACT_MADE;
+      feet_state_msg.states[i] = foot_state;
     }
-    realtime_joint_state_publisher_->unlockAndPublish();
-  }
-
-  if (realtime_dynamic_joint_state_publisher_ && realtime_dynamic_joint_state_publisher_->trylock()) {
-    auto& dynamic_joint_state_msg = realtime_dynamic_joint_state_publisher_->msg_;
-    dynamic_joint_state_msg.header.stamp = time;
-    for (size_t joint_index = 0; joint_index < dynamic_joint_state_msg.joint_names.size(); ++joint_index) {
-      const auto& name = dynamic_joint_state_msg.joint_names[joint_index];
-      for (size_t interface_index = 0;
-           interface_index < dynamic_joint_state_msg.interface_values[joint_index].interface_names.size();
-           ++interface_index) {
-        const auto& interface_name =
-            dynamic_joint_state_msg.interface_values[joint_index].interface_names[interface_index];
-        dynamic_joint_state_msg.interface_values[joint_index].values[interface_index] =
-            name_if_value_mapping_[name][interface_name];
-      }
-    }
-    realtime_dynamic_joint_state_publisher_->unlockAndPublish();
+    realtime_foot_state_publisher_->unlockAndPublish();
   }
 
   return controller_interface::return_type::OK;
