581a2514b7e250e2c111f938690db0046b838a69
Katie Hughes <khughes-bdai@theaiinstitute.com>
2025-03-17 17:40:45 -0400

    Get around namespacing issues
    Signed-off-by: Katie Hughes <khughes-bdai@theaiinstitute.com>

--- spot_controllers/src/foot_state_broadcaster.cpp
+++ spot_controllers/src/foot_state_broadcaster.cpp
@@ -58,24 +58,31 @@ controller_interface::InterfaceConfiguration FootStateBroadcaster::state_interfa
   controller_interface::InterfaceConfiguration state_interfaces_config;
 
   state_interfaces_config.type = controller_interface::interface_configuration_type::INDIVIDUAL;
-  //   for (const auto& joint : params_.joints) {
-  //     for (const auto& interface : params_.interfaces) {
-  //       state_interfaces_config.names.push_back(joint + "/" + interface);
-  //     }
-  //   }
+
+  // slight hack to avoid more config parsing -- get the namespace from the node to use as the sensor prefix.
+  const std::string node_namespace = get_node()->get_namespace();
+  // namespace is "/" if there is none, else it's "/<namespace>". Erase the first char ("/") for easier parsing.
+  const std::string trimmed_namespace = node_namespace.substr(1);
+  // Now trimmed_namespace is either the empty string or "<namespace>"
+  const std::string sensor_prefix = trimmed_namespace.empty() ? "" : trimmed_namespace + "/";
+  // Append this prefix to the sensor name
+  const std::string sensor_name = sensor_prefix + "foot_sensor";
+  RCLCPP_WARN(get_node()->get_logger(), "sensor name: %s", sensor_name.c_str());
+  // export state interfaces based on the prefix. They are hardcoded for the spot case.
+  state_interfaces_config.names.push_back(sensor_name + "/" + "foot_state.back.left");
+  state_interfaces_config.names.push_back(sensor_name + "/" + "foot_state.back.right");
+  state_interfaces_config.names.push_back(sensor_name + "/" + "foot_state.front.left");
+  state_interfaces_config.names.push_back(sensor_name + "/" + "foot_state.front.right");
   return state_interfaces_config;
 }
 
 controller_interface::CallbackReturn FootStateBroadcaster::on_configure(
     const rclcpp_lifecycle::State& /*previous_state*/) {
   params_ = param_listener_->get_params();
-  RCLCPP_INFO(get_node()->get_logger(), "CONFIGURING");
 
   try {
-    const std::string topic_name_prefix = "";
-
-    foot_state_publisher_ = get_node()->create_publisher<spot_msgs::msg::FootStateArray>(
-        topic_name_prefix + "feet_states", rclcpp::SystemDefaultsQoS());
+    foot_state_publisher_ =
+        get_node()->create_publisher<spot_msgs::msg::FootStateArray>("~/feet_states", rclcpp::SystemDefaultsQoS());
 
     realtime_foot_state_publisher_ =
         std::make_shared<realtime_tools::RealtimePublisher<spot_msgs::msg::FootStateArray>>(foot_state_publisher_);
@@ -112,15 +119,14 @@ controller_interface::CallbackReturn FootStateBroadcaster::on_deactivate(
 
 controller_interface::return_type FootStateBroadcaster::update(const rclcpp::Time& time,
                                                                const rclcpp::Duration& /*period*/) {
-  //   for (const auto& state_interface : state_interfaces_) {
-  //     std::string interface_name = state_interface.get_interface_name();
-  //     if (map_interface_to_joint_state_.count(interface_name) > 0) {
-  //       interface_name = map_interface_to_joint_state_[interface_name];
-  //     }
-  //     name_if_value_mapping_[state_interface.get_prefix_name()][interface_name] = state_interface.get_value();
-  //     RCLCPP_DEBUG(get_node()->get_logger(), "%s: %f\n", state_interface.get_name().c_str(),
-  //     state_interface.get_value());
-  //   }
+  // for (const auto& state_interface : state_interfaces_) {
+  //   std::string interface_name = state_interface.get_interface_name();
+  //   RCLCPP_WARN(get_node()->get_logger(), "%s: %f\n", state_interface.get_name().c_str(),
+  //   state_interface.get_value());
+  // }
+
+  // todo map the state interface above to the contact field below.
+  // important to check std::isnan() on the value, and pass in unknown if it is.
 
   if (realtime_foot_state_publisher_ && realtime_foot_state_publisher_->trylock()) {
     auto& feet_state_msg = realtime_foot_state_publisher_->msg_;
