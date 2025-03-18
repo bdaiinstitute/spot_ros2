d003cc564beef38566c5defe8703abfd979c97b5
Katie Hughes <khughes-bdai@theaiinstitute.com>
2025-03-18 10:46:23 -0400

    More clean up
    Signed-off-by: Katie Hughes <khughes-bdai@theaiinstitute.com>

--- spot_controllers/src/foot_state_broadcaster.cpp
+++ spot_controllers/src/foot_state_broadcaster.cpp
@@ -31,10 +31,6 @@ class State;
 }  // namespace rclcpp_lifecycle
 
 namespace spot_controllers {
-const auto kUninitializedValue = std::numeric_limits<double>::quiet_NaN();
-using hardware_interface::HW_IF_EFFORT;
-using hardware_interface::HW_IF_POSITION;
-using hardware_interface::HW_IF_VELOCITY;
 
 FootStateBroadcaster::FootStateBroadcaster() {}
 
@@ -59,7 +55,7 @@ controller_interface::InterfaceConfiguration FootStateBroadcaster::state_interfa
 
   state_interfaces_config.type = controller_interface::interface_configuration_type::INDIVIDUAL;
 
-  // slight hack to avoid more config parsing -- get the namespace from the node to use as the sensor prefix.
+  // Get the namespace from the node to use as the sensor prefix
   const std::string node_namespace = get_node()->get_namespace();
   // namespace is "/" if there is none, else it's "/<namespace>". Erase the first char ("/") for easier parsing.
   const std::string trimmed_namespace = node_namespace.substr(1);
@@ -67,12 +63,12 @@ controller_interface::InterfaceConfiguration FootStateBroadcaster::state_interfa
   const std::string sensor_prefix = trimmed_namespace.empty() ? "" : trimmed_namespace + "/";
   // Append this prefix to the sensor name
   const std::string sensor_name = sensor_prefix + "foot_sensor";
-  RCLCPP_WARN(get_node()->get_logger(), "sensor name: %s", sensor_name.c_str());
-  // export state interfaces based on the prefix. They are hardcoded for the spot case.
-  state_interfaces_config.names.push_back(sensor_name + "/" + "foot_state.back.left");
-  state_interfaces_config.names.push_back(sensor_name + "/" + "foot_state.back.right");
+  RCLCPP_DEBUG(get_node()->get_logger(), "sensor name: %s", sensor_name.c_str());
+  // export sensor interfaces. These are hardcoded, assuming the format from spot ros2 control tags
   state_interfaces_config.names.push_back(sensor_name + "/" + "foot_state.front.left");
   state_interfaces_config.names.push_back(sensor_name + "/" + "foot_state.front.right");
+  state_interfaces_config.names.push_back(sensor_name + "/" + "foot_state.back.left");
+  state_interfaces_config.names.push_back(sensor_name + "/" + "foot_state.back.right");
   return state_interfaces_config;
 }
 
@@ -117,23 +113,20 @@ controller_interface::CallbackReturn FootStateBroadcaster::on_deactivate(
   return CallbackReturn::SUCCESS;
 }
 
-controller_interface::return_type FootStateBroadcaster::update(const rclcpp::Time& time,
+controller_interface::return_type FootStateBroadcaster::update(const rclcpp::Time& /*time*/,
                                                                const rclcpp::Duration& /*period*/) {
-  // for (const auto& state_interface : state_interfaces_) {
-  //   std::string interface_name = state_interface.get_interface_name();
-  //   RCLCPP_WARN(get_node()->get_logger(), "%s: %f\n", state_interface.get_name().c_str(),
-  //   state_interface.get_value());
-  // }
-
-  // todo map the state interface above to the contact field below.
-  // important to check std::isnan() on the value, and pass in unknown if it is.
-
   if (realtime_foot_state_publisher_ && realtime_foot_state_publisher_->trylock()) {
     auto& feet_state_msg = realtime_foot_state_publisher_->msg_;
-
-    // update joint state message and dynamic joint state message
+    RCLCPP_WARN(get_node()->get_logger(), "UPDATE");
+    // update foot state message
     for (size_t i = 0; i < 4; ++i) {
-      feet_state_msg.states[i].contact = spot_msgs::msg::FootState::CONTACT_MADE;
+      // this follows the same order as in state_interface_configuration: FL, FR, BL, BL
+      const auto& state_interface = state_interfaces_.at(i);
+      const std::string interface_name = state_interface.get_interface_name();
+      const auto interface_value = state_interface.get_value();
+      uint8_t contact = std::isnan(interface_value) ? spot_msgs::msg::FootState::CONTACT_UNKNOWN : interface_value;
+      RCLCPP_WARN(get_node()->get_logger(), "%s: %f %d\n", interface_name.c_str(), interface_value, contact);
+      feet_state_msg.states[i].contact = contact;
     }
     realtime_foot_state_publisher_->unlockAndPublish();
   }
