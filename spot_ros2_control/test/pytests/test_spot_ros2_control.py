9c6acbb1743a198b898f9184f2a0d286c479e9ad
Katie Hughes <khughes-bdai@theaiinstitute.com>
2025-04-07 09:52:22 -0400

    Address review comments
    Signed-off-by: Katie Hughes <khughes-bdai@theaiinstitute.com>

--- spot_ros2_control/test/pytests/test_spot_ros2_control.py
+++ spot_ros2_control/test/pytests/test_spot_ros2_control.py
@@ -4,7 +4,7 @@ import pytest
 from bosdyn.api.robot_command_pb2 import RobotCommandResponse
 from bosdyn.api.robot_state_pb2 import RobotStateStreamResponse
 from sensor_msgs.msg import JointState
-from synchros2.futures import wait_for_future
+from synchros2.futures import unwrap_future
 from synchros2.scope import ROSAwareScope
 from synchros2.subscription import Subscription
 
@@ -41,25 +41,26 @@ def state_stream_response(position: list[float], velocity: list[float], load: li
 
 @pytest.mark.launch(fixture=pytest.robot_spot_ros2_control)
 def test_joint_states(simple_spot: SpotFixture, ros: ROSAwareScope) -> None:
-    # Mock a joint state stream response
+    # Create a state stream response containing unique joint state values for each joint
     position = list(range(0, NUM_JOINTS_ARM))
     velocity = list(range(NUM_JOINTS_ARM, 2 * NUM_JOINTS_ARM))
     load = list(range(2 * NUM_JOINTS_ARM, 3 * NUM_JOINTS_ARM))
     stream_response = state_stream_response(position=position, velocity=velocity, load=load)
+    # send this response to the mock robot
     simple_spot.api.GetRobotStateStream.future.returns(itertools.repeat(stream_response))
     response = RobotCommandResponse()
     response.status = RobotCommandResponse.Status.STATUS_OK
     simple_spot.api.RobotCommand.future.returns(response)
+    # start the mock joint control stream
     call = simple_spot.api.JointControlStream.serve(timeout=2.0)
     assert call is not None
+
+    # Grab the latest joint states message from the joint state broadcaster
     spot_name = simple_spot.api.name
-    # Grab the latest joint states topic
     joint_states = Subscription(JointState, f"/{spot_name}/low_level/joint_states", node=ros.node)
-    assert wait_for_future(joint_states.update, timeout_sec=10.0)
-    joint_state_message = joint_states.latest
-    assert joint_state_message is not None
+    joint_state_message = unwrap_future(joint_states.update, timeout_sec=10.0)
     expected_joints = [f"{spot_name}/{joint}" for joint in LEG_JOINTS + ARM_JOINTS]
-    # ensure that the joint state message contains the values from our state stream response.
+    # ensure that the joint state message contains the values from our state stream response
     for i, joint in enumerate(expected_joints):
         assert joint in joint_state_message.name
         joint_index = joint_state_message.name.index(joint)
@@ -67,7 +68,7 @@ def test_joint_states(simple_spot: SpotFixture, ros: ROSAwareScope) -> None:
         assert joint_state_message.velocity[joint_index] == velocity[i]
         assert joint_state_message.effort[joint_index] == load[i]
 
-    # Here for the SafePowerOff command when the hardware interface deactivates
+    # mock the response for the SafePowerOff command used when the hardware interface deactivates
     response = RobotCommandResponse()
     response.status = RobotCommandResponse.Status.STATUS_OK
     simple_spot.api.RobotCommand.future.returns(response)
